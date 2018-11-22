464a66f9f155abc73e454269a50294baeddc1c6f
Benjamin Becker <benj.becker@web.de>
2018-11-22 17:47:38 +0100

    0.2.4
    changes to meta data now possible via getChangeMeta, applyChangeMeta and a changeTable

--- R/02_data_cleaning.R
+++ R/02_data_cleaning.R
@@ -53,3 +53,113 @@ insert_string <- function(df, rows, col, string) {
   df
 }
 
+
+
+#### Change Meta data
+#############################################################################
+#' Extract table for Meta Data Changes.
+#'
+#' Function to obtain a data frame from a GADSdat object for meta data changes. Changes to value are currently not supported.
+#'
+#' The function ....
+#'
+#'@param GADSdat GADSdat object imported via eatGADS.
+#'@param changeCol Names of columns on which changes are to be specified.
+#'
+#'@return Returns the complete meta data sheet with additional columns for changes in meta data.
+#'
+#'@examples
+#'# Example data set
+#'to be done
+#'
+#'@export
+getChangeMeta <- function(GADSdat, changeCol = c("varName", "varLabel", "format", "display_width", "class", "value", "valLabel", "missings")) {
+  UseMethod("getChangeMeta")
+}
+
+#'@export
+getChangeMeta.GADSdat <- function(GADSdat, changeCol = c("varName", "varLabel", "format", "display_width",
+                                                         "class", "valLabel", "missings")) {
+  check_GADSdat(GADSdat)
+  if("value" %in% changeCol) stop("Changes to values are currently not supported. Remove 'value' from changeCol.")
+  if(!all(changeCol %in% names(GADSdat$labels))) stop("At least on variable name supplied in changeCol is not an actual column of the meta data table.", call. = FALSE)
+  newCols <- paste(changeCol, "_new", sep = "")
+  labels <- GADSdat$labels
+  for(n in newCols) labels[, n] <- NA
+  labels
+}
+
+
+#' Apply Meta Data Changes.
+#'
+#' Function to apply meta data changes to a GADSdat object specified by a change table extracted by getChangeMeta.
+#'
+#' The function ....
+#'
+#'@param GADSdat GADSdat object imported via eatGADS.
+#'@param changeTable Change table.
+#'
+#'@return Returns the complete meta data sheet with additional columns for changes in meta data.
+#'
+#'@examples
+#'# Example data set
+#'to be done
+#'
+#'@export
+applyChangeMeta <- function(GADSdat, changeTable) {
+  UseMethod("applyChangeMeta")
+}
+
+#'@export
+applyChangeMeta.GADSdat <- function(GADSdat, changeTable) {
+  check_GADSdat(GADSdat)
+  check_changeTable(GADSdat, changeTable)
+
+  dat <- GADSdat$dat
+  labels <- GADSdat$labels
+
+  # 01) variable names (changes in data and in meta data)
+  changeNameDF <- unique(changeTable[!is.na(changeTable[, "varName_new"]), c("varName", "varName_new")])
+  for(i in changeNameDF[, "varName"]) {
+    names(dat)[names(dat) == i] <- changeNameDF[changeNameDF$varName == i, "varName_new"]
+    labels[labels$varName == i, "varName"] <- changeNameDF[changeNameDF$varName == i, "varName_new"]
+  }
+
+  # 02)  for all but varNames and values (changes only in meta data)
+  change_vars <- grep("_new", names(changeTable), value = TRUE)
+  simpleChanges <- changeTable[, change_vars[!change_vars %in% c("varName_new")], drop = FALSE]
+  # NA in changeTable: value is kept; otherwise changed
+  for(i in names(simpleChanges)) {
+    oldName <- strsplit(i, "_")[[1]][1]
+    labels[, oldName] <- ifelse(is.na(simpleChanges[, i]), yes = labels[, oldName], no = simpleChanges[, i])
+  }
+
+  new_GADSdat(dat = dat, labels = labels)
+}
+
+
+# check change Table, also in relation to GADSdat object
+check_changeTable <- function(GADSdat, changeTable) {
+  oldChangeTable <- changeTable[, seq(ncol(GADSdat$labels))]
+  newChangeTable <- changeTable[, (ncol(GADSdat$labels)+1):ncol(changeTable), drop = FALSE]
+
+  if(!identical(GADSdat$labels, oldChangeTable)) stop("GADSdat and changeTable are not compatible. Columns without '_new' should not be changed in the changeTable.", call. = FALSE)
+
+  if(any(!grepl("_new", names(newChangeTable)))) stop("Illegal additional column names in changeTable.", call. = FALSE)
+  if("value_new" %in% names(newChangeTable)) stop("Illegal additional column names in changeTable.", call. = FALSE)
+
+  varLevel_vars <- names(newChangeTable[!grepl("valLabel|missings", names(newChangeTable))])
+  for(i in unique(changeTable$varName)) {
+    varLevel_df <- changeTable[changeTable$varName == i, varLevel_vars, drop = FALSE]
+    varLevel_df <- unique(varLevel_df)
+    if(ncol(varLevel_df) > 1) stop("Variable ", i, " has varying changes on variable level.", call. = FALSE)
+  }
+  return()
+}
+
+
+
+
+
+
+
--- tests/testthat/test_02_dataCleaning.R
+++ tests/testthat/test_02_dataCleaning.R
@@ -34,3 +34,49 @@ test_that("Missing codes are correctly checked and added", {
                "'miss' is inserted into column missings for 1 rows.\n")
   expect_equal(checkMissings(df4)$labels[2, "missings"], "miss")
 })
+
+
+### Meta changes
+# dfSAV <- import_spss(file = "c:/Benjamin_Becker/02_Repositories/packages/eatGADS/tests/testthat/helper_spss_missings.sav")
+dfSAV <- import_spss(file = "helper_spss_missings.sav")
+changes <- getChangeMeta(dfSAV, c("varName", "valLabel"))
+changes_var <- changes_val <- changes
+
+test_that("Extract meta change table", {
+  out <- c(names(df1$labels), "varName_new", "varLabel_new", "valLabel_new")
+  expect_equal(names(getChangeMeta(df1, changeCol = c("varName", "varLabel", "valLabel"))), out)
+  expect_equal(dim(getChangeMeta(df1)), c(2, 15))
+  expect_error(getChangeMeta(df1, changeCol = c("varName", "value")))
+  expect_error(getChangeMeta(df1, changeCol = c("varName", "vallabel")),
+               "At least on variable name supplied in changeCol is not an actual column of the meta data table.")
+})
+
+test_that("Check changeTable function", {
+  changes5 <- changes4 <- changes2 <- changes3 <- changes
+  changes2[, "some_changes"] <- NA
+  changes3[, "value_new"] <- NA
+  changes4[1, "varLabel"] <- "sth"
+  changes5[1, "varLabel_new"] <- "sth"
+  expect_error(applyChangeMeta(dfSAV, changes2), "Illegal additional column names in changeTable.")
+  expect_error(applyChangeMeta(dfSAV, changes3), "Illegal additional column names in changeTable.")
+  expect_error(applyChangeMeta(dfSAV, changes4), "GADSdat and changeTable are not compatible. Columns without '_new' should not be changed in the changeTable.")
+  expect_error(applyChangeMeta(dfSAV, changes5), "Variable VAR1 has varying changes on variable level.")
+})
+
+test_that("Changes to GADSdat on variable level", {
+  changes_var[1:3, "varName_new"] <- "new1"
+  g1 <- applyChangeMeta(dfSAV, changes_var)
+  expect_equal(g1$labels[, -1], dfSAV$labels[, -1])
+  expect_equal(g1$labels$varName, c(rep("new1", 3), rep("VAR2", 2), rep("VAR3", 2)))
+  expect_equal(names(g1$dat), c("new1", "VAR2", "VAR3"))
+})
+
+test_that("Changes to GADSdat on value level", {
+  changes_val[1, "valLabel_new"] <- "new_miss"
+  changes_val[2, "valLabel_new"] <- "new_miss2"
+  g2 <- applyChangeMeta(dfSAV, changes_val)
+  expect_equal(g2$labels[, -7], dfSAV$labels[, -7])
+  expect_equal(g2$labels$valLabel, c("new_miss", "new_miss2", "One", "missing", NA, "missing", NA))
+  expect_equal(names(g2$dat), names(dfSAV$dat))
+})
+
