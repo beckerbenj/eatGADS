6fc1ead0f596eae94be78153d592b441fedfa058
Benjamin Becker <benj.becker@web.de>
2019-05-24 11:57:14 +0200

    0.7.4
    changeVarLabels wrapper implemented

--- tests/testthat/test_02b_metadata_handling.R
+++ tests/testthat/test_02b_metadata_handling.R
@@ -253,3 +253,15 @@ test_that("Recode wrapper", {
   out2 <- recodeGADS(allG, varName = "VAR1", oldValues = c(1), newValues = c(10))
   expect_equal(out2$datList$dfSAV$VAR1, c(10, -99, -96, 2))
 })
+
+
+test_that("changevarlabel wrapper", {
+  out <- changeVarLabels(dfSAV, varName = "VAR1", varLabel = "test label")
+  expect_equal(out$labels$varLabel[1:3], rep("test label", 3))
+  expect_equal(out$dat, dfSAV$dat)
+
+  out <- changeVarLabels(dfSAV, varName = c("VAR3", "VAR2"), varLabel = c("label 3", "label 2"))
+  expect_equal(out$labels$varLabel, c(rep("Variable 1", 3), rep("label 2", 2), rep("label 3", 2)))
+  expect_equal(out$dat, dfSAV$dat)
+
+})
