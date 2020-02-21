9c6516a0dccac1091c4f6041cae0ced0f39da013
Benjamin Becker <benj.becker@web.de>
2020-02-21 17:26:53 +0100

    0.9.0
    value recoding roughly implemented

---
+++ R/15_recode.R
@@ -0,0 +1,177 @@
+#### Write xlsx for recoding
+#############################################################################
+#' Recode values.
+#'
+#' Extract values form variables of \code{GADSdat} object and write to \code{xlsx} for manuel recoding.
+#'
+#' If recoding of one or multiple variables is more complex, a lookup table can be created for later importing via \code{eatGADS}.
+#'
+#'@param GADSdat A \code{GADSdat} object.
+#'@param recodeVars Character vector of variable names which should be recoded.
+#'@param addCols Character vector of additional column names for recoding purposes.
+#'@param sort_by By which column should the long format data.frame be sorted? If \code{NULL}, no sorting is performed.
+#'
+#'@return Returns a data frame in long format including all unique values of the variables in \code{recodeVars}.
+#'
+#'@examples
+#'\dontrun{
+#'#to be done
+#'
+#'@export
+createLookup <- function(GADSdat, recodeVars, sort_by = NULL, addCols = c("newValue")) {
+  UseMethod("createLookup")
+}
+
+#'@export
+createLookup.GADSdat <- function(GADSdat, recodeVars, sort_by = NULL, addCols = c("newValue")) {
+  check_GADSdat(GADSdat)
+  if(!all(recodeVars %in% namesGADS(GADSdat))) stop("Some of the variables are not variables in the GADSdat.")
+  vars_w <- data.table::as.data.table(GADSdat$dat[, recodeVars, drop = FALSE])
+  dt_l <- unique(data.table::melt(vars_w, measure.vars = recodeVars, variable.factor = FALSE, value.factor = FALSE))
+
+  if(!all(sort_by %in% names(dt_l))) stop("data.frame can only be sorted by 'variable' or 'value' or both.")
+  if(!is.null(sort_by)) {
+    data.table::setorderv(dt_l, cols = sort_by, order=1L, na.last=FALSE)
+  }
+
+  vars_l <- data.frame(dt_l, stringsAsFactors = FALSE)
+  stopifnot(length(addCols) >= 1)
+  for(i in addCols) vars_l[, i] <- NA
+
+  vars_l
+}
+
+
+#### Collapse multiple recodings
+#############################################################################
+#' Collapse columns.
+#'
+#' Extract values form variables of \code{GADSdat} object and write to \code{xlsx} for manuel recoding.
+#'
+#' If recoding of one or multiple variables is more complex, a lookup table can be created for later importing via \code{eatGADS}.
+#'
+#'@param GADSdat A \code{GADSdat} object.
+#'@param recodeVars Character vector of variable names which should be recoded.
+#'@param addCols Character vector of additional column names for recoding purposes.
+#'@param sort_by By which column should the long format data.frame be sorted? If \code{NULL}, no sorting is performed.
+#'
+#'@return Returns a data frame in long format including all unique values of the variables in \code{recodeVars}.
+#'
+#'@examples
+#'\dontrun{
+#'#to be done
+#'
+#'@export
+collapseColumns <- function(lookUp, recodeVars, prioritize) {
+  if(length(recodeVars) != 2) stop("More recode variables than 2 are currently not supported.")
+  if(length(prioritize) != 1) stop("Prioritize must be of length = length(recodeVars) - 1.")
+  lookUp[, "valueNew"] <- ifelse(is.na(lookUp[[prioritize]]),
+                                 yes = lookUp[[recodeVars[!recodeVars %in% prioritize]]],
+                                 no = lookUp[[prioritize]])
+  lookUp[, names(lookUp)[!names(lookUp) %in% recodeVars]]
+}
+
+
+#### Apply recode lookup table
+#############################################################################
+#' Recoade via lookup table.
+#'
+#' Extract values form variables of \code{GADSdat} object and write to \code{xlsx} for manuel recoding.
+#'
+#' If recoding of one or multiple variables is more complex, a lookup table can be created for later importing via \code{eatGADS}.
+#'
+#'@param GADSdat A \code{GADSdat} object.
+#'@param lookUp LookUp table created by \code{\link{createLookup}} and - if necessary -  collapsed by \code{\link{collapseColumns}}.
+#'
+#'@return Returns a recoded \code{GADSdat}.
+#'
+#'@examples
+#'\dontrun{
+#'#to be done
+#'
+#'@export
+applyLookup <- function(GADSdat, lookup, suffix = "_r") {
+  UseMethod("applyLookup")
+}
+
+#'@export
+applyLookup.GADSdat <- function(GADSdat, lookup, suffix = "_r") {
+  check_GADSdat(GADSdat)
+  check_lookup(lookup, GADSdat)
+
+  rec_vars <- unique(lookup[["variable"]])
+
+  GADSdat2 <- GADSdat
+  ## recode via data.table
+  for(nam in rec_vars) {
+    rec_dt <- data.table::as.data.table(GADSdat2$dat)
+
+    sub_lu <- lookup[lookup$variable == nam, c("value", "newValue")]
+    names(sub_lu) <- c(nam, "valueNew")
+    sub_lu <- eatTools::asNumericIfPossible(sub_lu, force.string = FALSE)
+    new_nam <- paste0(nam, suffix)
+    rec_dt[sub_lu, on = nam, (new_nam) := i.valueNew]
+
+    GADSdat2 <- updateMeta(GADSdat2, newDat = as.data.frame(rec_dt, stringsAsFactor = FALSE))
+    GADSdat2 <- reuseMeta(GADSdat = GADSdat2, varName = new_nam, other_GADSdat = GADSdat, other_varName = nam)
+  }
+
+  check_GADSdat(GADSdat2)
+  GADSdat2
+}
+
+check_lookup <- function(lookup, GADSdat) {
+  if(!all(lookup$variable %in% namesGADS(GADSdat))) stop("Some of the variables are not variables in the GADSdat.")
+  if(!identical(names(lookup), c("variable", "value", "newValue"))) stop("LookUp table has to be formatted correctly.")
+
+  # tbd!!!!!!!!!!
+}
+
+
+#### Apply recode lookup table
+#############################################################################
+#' Recoade via lookup table.
+#'
+#' Extract values form variables of \code{GADSdat} object and write to \code{xlsx} for manuel recoding.
+#'
+#' If recoding of one or multiple variables is more complex, a lookup table can be created for later importing via \code{eatGADS}.
+#'
+#'@param GADSdat A \code{GADSdat} object.
+#'@param lookUp LookUp table created by \code{\link{createLookup}} and - if necessary -  collapsed by \code{\link{collapseColumns}}.
+#'
+#'@return Returns a recoded \code{GADSdat}.
+#'
+#'@examples
+#'\dontrun{
+#'#to be done
+#'
+#'@export
+collapseMC_Text <- function(GADSdat, mc_var, text_var, mc_code4text, suffix = "_r") {
+  UseMethod("collapseMC_Text")
+}
+
+#'@export
+collapseMC_Text.GADSdat <- function(GADSdat, mc_var, text_var, mc_code4text, suffix = "_r") {
+  MC_dat <- GADSdat$dat[, mc_var, drop = FALSE]
+  #suppressMessages(MC_gads <- updateMeta(GADSdat, MC_dat))
+  MC_gads <- updateMeta(GADSdat, MC_dat)
+  MC <- extractData(MC_gads, convertMiss = FALSE, convertLabels = "character")[, 1]
+  MC[which(MC == mc_code4text)] <- NA
+
+  MC_new <- ifelse(is.na(MC), yes = GADSdat$dat[[text_var]],
+                                 no = MC)
+  df_MC_new <- data.frame(MC_new, stringsAsFactors = TRUE)
+  names(df_MC_new) <- paste0(mc_var, suffix)
+  MC_gads2 <- import_DF(df_MC_new)
+
+  GADSdat_dat <- cbind(GADSdat$dat, MC_gads2$dat)
+  GADSdat_out <- updateMeta(GADSdat, GADSdat_dat)
+  GADSdat_out <- reuseMeta(GADSdat_out, other_GADSdat = MC_gads2, varName = paste0(mc_var, suffix))
+
+  GADSdat_out
+}
+
+
+
+
+
---
+++ tests/testthat/test_15_recode.R
@@ -0,0 +1,96 @@
+
+context("Value recoding via Excel")
+
+
+################# Create lookup table ---------------------------------------------------
+# rawDat <- import_spss(file = "c:/Benjamin_Becker/02_Repositories/packages/eatGADS/tests/testthat/helper_spss.sav")
+rawDat <- import_spss("helper_spss.sav")
+
+test_that("Create lookup for 1 character variables",{
+  test <- createLookup(rawDat, recodeVars = c("VAR1"))
+  expect_equal(test$variable, c("VAR1"))
+  expect_equal(dim(test), c(1, 3))
+})
+
+test_that("Create lookup for 2 numeric variables",{
+  test <- createLookup(rawDat, recodeVars = c("VAR1", "VAR2"), addCols = c("Rater_1", "Rater_2"))
+  expect_equal(test$variable, c("VAR1", "VAR2"))
+  expect_equal(names(test), c("variable", "value", "Rater_1", "Rater_2"))
+})
+
+test_that("Create lookup for 2 mixed variables",{
+  test <- suppressWarnings(createLookup(rawDat, recodeVars = c("VAR1", "VAR3")))
+  expect_equal(test$variable, c("VAR1", "VAR3"))
+  expect_equal(test$value, c(1, "a"))
+  expect_equal(test$newValue, c(NA, NA))
+  expect_equal(dim(test), c(2, 3))
+})
+
+# testM <- import_spss("c:/Benjamin_Becker/02_Repositories/packages/eatGADS/tests/testthat/helper_spss_missings.sav")
+testM <- import_spss("helper_spss_missings.sav")
+
+lu1 <- createLookup(testM, recodeVars = c("VAR1", "VAR2"), addCols = c("r1", "r2"))
+
+test_that("Test unique values functionality for Create lookups",{
+  expect_equal(lu1$value, c(1, -99, -96, 2, 1))
+  #expect_equal(test$value, c(1, 3))
+  #expect_equal(test$newValue, c(NA, NA))
+  #expect_equal(dim(test), c(2, 3))
+})
+
+lu2 <- createLookup(testM, recodeVars = c("VAR1"), sort_by = "value")
+lu3 <- createLookup(testM, recodeVars = c("VAR1", "VAR2"), sort_by = "value")
+test_that("Ordering by values",{
+  expect_equal(lu2$value, c(-99, -96, 1, 2))
+
+  expect_equal(lu3$value, c(-99, -96, 1, 1, 2))
+
+  test2 <- createLookup(testM, recodeVars = c("VAR1", "VAR2"), sort_by = c("variable", "value"))
+  expect_equal(test2$value, c(-99, -96, 1, 2, 1))
+})
+
+
+################# Recode based on lookup table ---------------------------------------------------
+test_that("Test unique values functionality for Create lookups",{
+  lu1$r1 <- c(1, 2, NA, 4, NA)
+  lu1$r2 <- c(NA, -2, 3, 4, 5)
+
+  lu_r <- collapseColumns(lu1, recodeVars = c("r1", "r2"), prioritize = "r2")
+  expect_equal(lu_r$valueNew, c(1, -2, 3, 4, 5))
+})
+
+test_that("Tests for formatting of lookup",{
+  lu_false <- lu1[, 2:4]
+  expect_error(ng <- applyLookup(testM, lu_false))
+})
+
+test_that("Applying recode for 1 variable",{
+  lu2$newValue <- c(-9, -6, 10, 11)
+  ng <- applyLookup(testM, lu2)
+  expect_equal(ng$dat$VAR1_r, c(10, -9, -6, 11))
+})
+
+test_that("Applying recode for more variables",{
+  lu3$newValue <- c(-9, -6, 10, -10, 11)
+  ng <- applyLookup(testM, lu3)
+  expect_equal(ng$dat$VAR1_r, c(10, -9, -6, 11))
+  expect_equal(ng$dat$VAR2_r, rep(-10, 4))
+})
+
+
+
+################# Combine MC and text ---------------------------------------------------
+mc <- as.factor(c("Ger", "other", "other", "Aus"))
+mt <- data.frame(ID = 1:4, mc = mc, text = c(NA, "Eng", "Aus", "Aus2"), stringsAsFactors = FALSE)
+mt_gads <- import_DF(mt)
+
+test_that("Combine mc and text",{
+  test <- collapseMC_Text(mt_gads, mc_var = "mc", text_var = "text", mc_code4text = "other")
+  expect_equal(test$dat$mc_r, c(3, 2, 1, 1))
+  test_dat <- extractData(test)
+  expect_equal(test_dat$mc_r, c("Ger", "Eng", "Aus", "Aus"))
+})
+
+
+
+
