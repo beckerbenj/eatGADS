a995447d7fbcc33aaeb2f402135f79ea0a6660a5
Benjamin Becker <benj.becker@web.de>
2020-04-09 19:39:59 +0200

    0.11.0
    numChecks

---
+++ R/17_checks.R
@@ -0,0 +1,102 @@
+
+
+#### Create data.frame for plausability check of numerical variables
+#############################################################################
+#' Create data.frame for specification of numerical plausability checks.
+#'
+#' All numerical variables without value labels in a \code{GADSdat} are selected and a \code{data.frame} is created, which allows the specification of minima and maxima.
+#'
+#' tbd
+#'
+#'@param GADSdat A \code{GADSdat} object.
+#'
+#'@return A data.frame with the following variables:
+#'\item{variable}{All numerical variables in the \code{GADSdat}}
+#'\item{varLabel}{Corresponding variable labels}
+#'\item{min}{Minimum value for the specific variable.}
+#'\item{max}{Maximum value for the specific variable.}
+#'\item{value_new}{Which value should be inserted if values exceed the specified range?}
+#'
+#'@examples
+#' # tbd
+#'
+#'@export
+createNumCheck <- function(GADSdat) {
+  UseMethod("createNumCheck")
+}
+
+#'@export
+createNumCheck.GADSdat <- function(GADSdat) {
+  check_GADSdat(GADSdat)
+
+  all_names <- namesGADS(GADSdat)
+  all_meta <- extractMeta(GADSdat)
+  labeled_names <- unique(all_meta[(is.na(all_meta$missings) | all_meta$missings != "miss") & !is.na(all_meta$value), "varName"])
+  txt_names <- all_names[sapply(GADSdat$dat, is.character)]
+
+  num_names <- all_names[!all_names %in% c(labeled_names, txt_names)]
+  num_varLabels <- sapply(num_names, function(x) unique(extractMeta(GADSdat, x)[, "varLabel"]))
+
+  data.frame(variable = num_names,
+             varLabel = num_varLabels,
+             min = NA, max = NA,
+             value_new = NA,
+             stringsAsFactors = FALSE)
+}
+
+
+
+#### Apply num check
+#############################################################################
+#' Apply recodes according to a numerical check data.frame.
+#'
+#' Applies recodes as specified by a \code{numCheck} \code{data.frame}, as created by \code{\link{createNumCheck}}.
+#'
+#' tbd
+#'
+#'@param GADSdat A \code{GADSdat} object.
+#'
+#'@return A recoded \code{GADSdat}.
+#'
+#'@examples
+#' # tbd
+#'
+#'@export
+applyNumCheck <- function(GADSdat, numCheck) {
+  UseMethod("applyNumCheck")
+}
+
+#'@export
+applyNumCheck.GADSdat <- function(GADSdat, numCheck) {
+  check_GADSdat(GADSdat)
+  check_numCheck(GADSdat, numCheck)
+
+  for(varName in numCheck$variable) {
+    new_value <- numCheck[numCheck$variable == varName, "value_new"]
+    min_value <- numCheck[numCheck$variable == varName, "min"]
+    max_value <- numCheck[numCheck$variable == varName, "max"]
+
+    #browser()
+
+    GADSdat$dat[which(GADSdat$dat[[varName]] < min_value), varName] <- new_value
+    GADSdat$dat[which(GADSdat$dat[[varName]] > max_value), varName] <- new_value
+  }
+
+  GADSdat
+}
+
+check_numCheck <- function(GADSdat, numCheck) {
+  if(!is.data.frame(numCheck)) stop("numCheck needs to be a data.frame created by createNumCheck")
+  if(!all(numCheck$variable %in% namesGADS(GADSdat))) stop("Not all variables in numCheck are variables in the GADSdat.")
+  if(!identical(names(numCheck), c("variable", "varLabel", "min", "max", "value_new"))) stop("numCheck is formatted incorrectly. Check column names.")
+  if(is.character(numCheck$min)) stop("Column 'min' containts non numeric values.")
+  if(is.character(numCheck$max)) stop("Column 'max' containts non numeric values.")
+  if(is.character(numCheck$value_new)) stop("Column 'value_new' containts non numeric values.")
+
+  if(any(duplicated(numCheck$variable))) stop("numCheck containts duplicated rows.")
+  return()
+}
+
+
+
+
---
+++ tests/testthat/test_17_checks.R
@@ -0,0 +1,44 @@
+context("Recode multi mc based on text variables")
+
+
+################# Match values and variables labels ---------------------------------------------------
+mt2 <- data.frame(num1 = 1:4, num2 = c(14, 50, 13, NA), fac1 = factor(NA, "Eng", "Aus", "Aus2"), text2 = c(NA, "Franz", NA, "Ger"),stringsAsFactors = FALSE)
+mt2_gads <- import_DF(mt2)
+
+# dfSAV <- import_spss(file = "c:/Benjamin_Becker/02_Repositories/packages/eatGADS/tests/testthat/helper_spss_missings.sav")
+dfSAV <- import_spss(file = "helper_spss_missings.sav")
+
+
+test_that("Create minmax data.frame", {
+  out <- createNumCheck(mt2_gads)
+  expect_equal(out$variable, c("num1", "num2"))
+
+  out <- createNumCheck(dfSAV)
+  expect_equal(out$variable, c("VAR3"))
+  expect_equal(out$varLabel, c("Variable 3"))
+})
+
+test_that("check numCheck", {
+  nc <- createNumCheck(mt2_gads)
+  nc1 <- nc2 <- nc3 <- nc4 <- nc
+
+  nc1$variable <- c("ID", 14)
+  expect_error(check_numCheck(mt2_gads, nc1), "Not all variables in numCheck are variables in the GADSdat.")
+  nc2$min <- c("ID", 14)
+  expect_error(check_numCheck(mt2_gads, nc2), "Column 'min' containts non numeric values.")
+  nc3$value_new <- c("ID", 14)
+  expect_error(check_numCheck(mt2_gads, nc3), "Column 'value_new' containts non numeric values.")
+
+
+})
+
+test_that("Apply minmax data.frame", {
+  nc <- createNumCheck(mt2_gads)
+  nc$min <- c(2, 14)
+  nc$max <- c(NA, 50)
+  nc$value_new <- c(NA, -99)
+
+  out <- applyNumCheck(mt2_gads, nc)
+  expect_equal(out$dat$num1, c(NA, 2, 3, 4))
+  expect_equal(out$dat$num2, c(14, 50, -99, NA))
+})
