7d87dd49ca6145bf5f808651e6a3e850e212e444
Benjamin Becker <benj.becker@web.de>
2020-08-10 17:01:21 +0200

    0.14.11
    recodeGADS (docu) fixes (Mail JB)

--- R/recodeGADS.R
+++ R/recodeGADS.R
@@ -1,22 +1,43 @@
 #### Recode variable
 #############################################################################
-#' Recode a variable.
+#' Recode a labeled variable.
 #'
-#' Recode a variable as part of a \code{GADSdat} or \code{all_GADSdat} object.
+#' Recode a labeled variable as part of a \code{GADSdat} or \code{all_GADSdat} object.
 #'
-#' Applied to a \code{GADSdat} or \code{all_GADSdat} object, this function is a wrapper of \code{\link{getChangeMeta}} and \code{\link{applyChangeMeta}}
+#' Applied to a \code{GADSdat} or \code{all_GADSdat} object, this function is a wrapper of \code{\link{getChangeMeta}} and \code{\link{applyChangeMeta}}.
+#' \code{oldValues} and \code{newValues} are matched by ordering in the function call.
+#'
+#' Functionality including \code{newValueLabels} is still very experimental. The argument can be used to simultaneously change value labels. However,
+#' all existing (new) values need to be assigned a new label.
+#'
+#' For recoding character variables, using lookup tables via \code{\link{createLookup}} is recommended.
 #'
 #'@param GADSdat \code{GADSdat} object imported via \code{eatGADS}.
 #'@param varName Name of the variable to be recoded.
 #'@param oldValues Vector containing the old values.
-#'@param newValues Vector containing the new values, in identical order as \code{oldNames}.
-#'@param newValueLabels Named vector containing new value labels for the new values. All new values have to get labels. Slightly experimental implementation.
+#'@param newValues Vector containing the new values (in the respective order as \code{oldValues}).
+#'@param newValueLabels [optional] Named vector containing new value labels for the new values.
+#' All new values have to get labels. Very experimental implementation.
 #'
-#'@return Returns the \code{GADSdat} object with changed variable names.
+#'@return Returns a \code{GADSdat}.
 #'
 #'@examples
-#'# Example data set
-#'#to be done
+#'# Example gads
+#'example_df <- data.frame(ID = 1:5, color = c("blue", "blue", "green", "other", "other"),
+#'                         animal = c("dog", "Dog", "cat", "hors", "horse"),
+#'                         age = c(NA, 16, 15, 23, 50),
+#'                         stringsAsFactors = FALSE)
+#'example_df$animal <- as.factor(example_df$animal)
+#'gads <- import_DF(example_df)
+#'
+#'# simple recode
+#'gads2 <- recodeGADS(gads, varName = "animal",
+#'                    oldValues = c(3, 4), newValues = c(2, 5))
+#'
+#'# recode while changing value labels
+#'gads3 <- recodeGADS(gads, varName = "animal",
+#'                    oldValues = c(1, 3, 4), newValues = c(1, 2, 5),
+#'                    newValueLabels = c('1' = "cats", '2' = "dogs", '5' = "horses"))
 #'
 #'@export
 recodeGADS <- function(GADSdat, varName, oldValues, newValues, newValueLabels = NULL) {
@@ -25,6 +46,7 @@ recodeGADS <- function(GADSdat, varName, oldValues, newValues, newValueLabels = 
 #'@export
 recodeGADS.GADSdat <- function(GADSdat, varName, oldValues, newValues, newValueLabels = NULL) {
   checkRecodeVectors(oldValues = oldValues, newValues = newValues, varName = varName, dat = GADSdat$dat)
+  if(all(is.na(GADSdat$labels[GADSdat$labels$varName == varName, "value"]))) stop("'varName' needs to be a labeled variable in the GADS.")
   changeTable <- getChangeMeta(GADSdat, level = "value")
   for(i in seq_along(oldValues)) {
     changeTable[changeTable$varName == varName & changeTable$value == oldValues[i], "value_new"] <- newValues[i]
@@ -65,13 +87,13 @@ recodeGADS.all_GADSdat <- function(GADSdat, varName, oldValues, newValues, newVa
 
 checkRecodeVectors <- function(oldValues, newValues, varName, dat) {
   if(length(oldValues) != length(newValues)) stop("oldValues and newValues are not of identical length.", call. = FALSE)
-  if(!varName %in% names(dat)) stop("varName is not a real variable name.", call. = FALSE)
+  if(!varName %in% names(dat)) stop("'varName' is not a real variable name.", call. = FALSE)
   return()
 }
 
 checkNewValueLabels <- function(newValueLabels, newValues) {
   if(!is.character(newValueLabels)) stop("newValueLabels is not a character.")
   if(any(duplicated(names(newValueLabels)))) stop("Duplicated values in newValueLabels.")
-  if(length(names(newValueLabels)) == 0) stop("neValueLabels needs to be named.")
+  if(length(names(newValueLabels)) == 0) stop("newValueLabels needs to be named.")
   compare_and_order(set1 = names(newValueLabels), set2 = unique(newValues), FUN = stop)
 }
--- tests/testthat/test_recodeGADS.R
+++ tests/testthat/test_recodeGADS.R
@@ -12,11 +12,20 @@ test_that("Recode wrapper", {
   expect_equal(out2$datList$dfSAV$VAR1, c(10, -99, -96, 2))
 })
 
+test_that("Recode wrapper errors", {
+  df <- data.frame(v1 = 1:2, v2 = c("a", "b"), stringsAsFactors = FALSE)
+  g <- import_DF(df)
+  expect_error(recodeGADS(g, varName = "v3", oldValues = c(1), newValues = c(10)), "'varName' is not a real variable name.")
+  expect_error(recodeGADS(g, varName = "v2", oldValues = c(1), newValues = c(10)), "'varName' needs to be a labeled variable in the GADS.")
+
+})
+
+
 test_that("Recode wrapper with new value labels", {
   expect_error(recodeGADS(dfSAV, varName = "VAR1", oldValues = c(1), newValues = c(10), newValueLabels = c('10' = "la", '11' = "muh")), "The following variables are not in set2: 11")
   expect_error(recodeGADS(dfSAV, varName = "VAR1", oldValues = c(1, 2), newValues = c(10, 11), newValueLabels = c('11' = "muh")), "The following variables are not in set1: 10")
   expect_error(recodeGADS(dfSAV, varName = "VAR1", oldValues = c(1), newValues = c(10), newValueLabels = c('10' = "muh")), "The following variables are not in set1: -99, -96")
-  expect_error(recodeGADS(dfSAV, varName = "VAR1", oldValues = c(1), newValues = c(10), newValueLabels = c("muh")), "neValueLabels needs to be named.")
+  expect_error(recodeGADS(dfSAV, varName = "VAR1", oldValues = c(1), newValues = c(10), newValueLabels = c("muh")), "newValueLabels needs to be named.")
 
   out <- recodeGADS(dfSAV, varName = "VAR1", oldValues = c(1, -99, -96), newValues = c(1, -9, -9), newValueLabels = c('1' = "new_one", '-9' = "new_miss"))
   expect_equal(nrow(out$labels), 6)
