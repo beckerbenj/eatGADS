78e4c9daede15d932a8eca5bb9bacc7f93ea04db
Benjamin Becker <benj.becker@web.de>
2020-11-13 16:41:30 +0100

    fixed pisa doi

--- R/pisa.R
+++ R/pisa.R
@@ -12,5 +12,5 @@
 #'
 #' @docType data
 #' @keywords datasets
-#' @source Research Data Center at the Institute for Educational Quality Improvement (2020). Programme for International Student Assessment - Plus 2012, 2013 (PISA Plus 2012-2013) - Campus File (Version 1) [Data set]. Berlin: Institute for Educational Quality Improvement. \url{http://doi.org/10.5159/IQB_PISA_Plus_2012-13_CF_v1}
+#' @source Research Data Center at the Institute for Educational Quality Improvement (2020). Programme for International Student Assessment - Plus 2012, 2013 (PISA Plus 2012-2013) - Campus File (Version 1) [Data set]. Berlin: Institute for Educational Quality Improvement. \doi{https://doi.org/10.5159/IQB_PISA_Plus_2012-13_CF_v1}
 "pisa"
