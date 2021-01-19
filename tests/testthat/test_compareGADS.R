acbc08223d65d38218ee28ef873df7b16cfe54cc
Ben <benj.becker@web.de>
2021-01-19 18:49:35 +0100

    add stringsAsFactors in compareGADS

--- tests/testthat/test_compareGADS.R
+++ tests/testthat/test_compareGADS.R
@@ -16,7 +16,8 @@ test_that("Compare GADS", {
 
   expect_equal(out[[1]], "all equal")
   expect_equal(out[[2]], data.frame(value = c("3", "5"), frequency = c(1, 1),
-                                    valLabel = c(NA_character_, NA), missings = c(NA_character_, NA)))
+                                    valLabel = c(NA_character_, NA), missings = c(NA_character_, NA),
+                                    stringsAsFactors = FALSE))
 
   dfSAV_b <- dfSAV
   dfSAV_b$dat[, 1] <- c(9, 9, 9, 9)
@@ -24,8 +25,11 @@ test_that("Compare GADS", {
   out2 <- compareGADS(dfSAV, dfSAV_b, varNames = namesGADS(dfSAV))
 
   expect_equal(out2[["VAR1"]], data.frame(value = c("-99", "-96", "1", "2"), frequency = rep(1, 4),
-                                     valLabel = c("By design", "Omission", "One", NA), missings = c("miss", "miss", "valid", NA)))
+                                     valLabel = c("By design", "Omission", "One", NA),
+                                     missings = c("miss", "miss", "valid", NA),
+                                     stringsAsFactors = FALSE))
   expect_equal(out2[["VAR2"]], "all equal")
   expect_equal(out2[[3]], data.frame(value = c("-98", "1"), frequency = c(1, 3),
-                                     valLabel = NA_character_, missings = c("miss", NA)))
+                                     valLabel = NA_character_, missings = c("miss", NA),
+                                     stringsAsFactors = FALSE))
 })
