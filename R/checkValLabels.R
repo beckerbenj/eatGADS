290fe94b1b986bede1d9e93fcd9762a873d08b68
Ben <benj.becker@web.de>
2021-02-08 16:52:07 +0100

    range for checkValLabels

--- R/checkValLabels.R
+++ R/checkValLabels.R
@@ -8,6 +8,8 @@
 #'
 #'@param GADSdat A \code{GADSdat} object.
 #'@param vars Character vector with the variable names to which \code{checkValLabels()} should be applied.
+#'@param valueRange [optional] Numeric vector of length 2: In which range should numeric values be checked? If specified, only numeric values
+#'are returned and strings are omitted.
 #'
 #'@return Returns a list of length 2. Each contains a list of the length of \code{vars}.
 #'\item{labels with no values}{Value labels with no occurrence in the data}
@@ -17,13 +19,17 @@
 #'# Check a categorical and a metric variable
 #'checkValLabels(pisa, vars = c("g8g9", "age"))
 #'
+#' # Check while defining a specific value range
+#'checkValLabels(pisa, vars = c("g8g9", "age", "idschool"),
+#'               valueRange = c(0, 5))
+#'
 #'@export
-checkValLabels <- function(GADSdat, vars = namesGADS(GADSdat)) {
+checkValLabels <- function(GADSdat, vars = namesGADS(GADSdat), valueRange = NULL) {
   UseMethod("checkValLabels")
 }
 
 #'@export
-checkValLabels.GADSdat <- function(GADSdat, vars = namesGADS(GADSdat)) {
+checkValLabels.GADSdat <- function(GADSdat, vars = namesGADS(GADSdat), valueRange = NULL) {
   check_GADSdat(GADSdat)
   check_vars_in_GADSdat(GADSdat, vars = vars)
 
@@ -36,7 +42,20 @@ checkValLabels.GADSdat <- function(GADSdat, vars = namesGADS(GADSdat)) {
     label_no_values[[i]] <- setdiff(i_labeled_values, i_real_values)
     not_labeled[[i]] <- setdiff(i_real_values, i_labeled_values)
   }
+  out <- list("labels with no values" = label_no_values, "not labeled values" = not_labeled)
 
-  list("labels with no values" = label_no_values, "not labeled values" = not_labeled)
+  if(!is.null(valueRange)) {
+    if(!is.numeric(valueRange) || length(valueRange) != 2) stop("'valueRange' needs to be a numeric vector of length 2.")
+
+    out <- lapply(out, function(x) {
+      lapply(x, function(single_vector){
+        single_vector <- suppressWarnings(eatTools::asNumericIfPossible(single_vector))
+        single_vector <- na_omit(single_vector)
+        single_vector[between(single_vector, range(valueRange)[1], range(valueRange)[2])]
+      })
+    })
+  }
+
+  out
 }
 
--- tests/testthat/test_checkValLabels.R
+++ tests/testthat/test_checkValLabels.R
@@ -10,6 +10,10 @@ df3$dat[1, 1:2] <- 8
 test_that("Input validation", {
   expect_error(checkValLabels(df1, vars = 3:4),
                "The following 'vars' are not variables in the GADSdat: 3, 4")
+  expect_error(checkValLabels(df1, valueRange = 3:5),
+               "'valueRange' needs to be a numeric vector of length 2.")
+  expect_error(checkValLabels(df1, valueRange = letters[3:4]),
+               "'valueRange' needs to be a numeric vector of length 2.")
 })
 
 test_that("Standard case", {
@@ -33,4 +37,19 @@ test_that("With NAs", {
   expect_equal(out[[1]][[3]], c(-99))
   expect_equal(out[[2]][[1]], c(2))
   expect_equal(out[[2]][[3]], numeric(0))
+})
+
+
+test_that("with specific value range", {
+  out <- checkValLabels(dfSAV, valueRange = c(-100, 0))
+  expect_equal(names(out), c("labels with no values", "not labeled values"))
+  expect_equal(names(out[[1]]), paste0("VAR", 1:3))
+  expect_equal(out[[1]][[1]], numeric(0))
+  expect_equal(out[[1]][[2]], c(-96, -99))
+  expect_equal(out[[1]][[3]], c(-99))
+  expect_equal(out[[2]][[1]], numeric())
+  expect_equal(out[[2]][[3]], numeric())
+
+  out2 <- checkValLabels(dfSAV, valueRange = c(0, -100))
+  expect_equal(out2, out)
 })
