17998f83b53737f7f943195ced6d64d55138aa5b
Karoline Sachse <sachseka@user.hu-berlin.de>
2021-04-08 19:12:30 +0200

    first full version of write_spss2

--- R/write_spss2.R
+++ R/write_spss2.R
@@ -15,27 +15,69 @@
 #'
 #'
 #'@export
-write_spss2 <- function(GADSdat, filePath, syntaxPath) {
+write_spss2 <- function(GADSdat, filePath, syntaxPath, dec = ".") {
   UseMethod("write_spss2")
 }
 
 #'@export
-write_spss2.GADSdat <- function(GADSdat, filePath, syntaxPath) {
+write_spss2.GADSdat <- function(GADSdat, filePath, syntaxPath, dec =".") {
 
   ## write txt
   utils::write.table(GADSdat$dat, file = filePath, row.names = FALSE, col.names = FALSE,
-              sep = "\t", dec = ".", quote = FALSE, na = "", eol = "\n")
+              sep = "\t", dec = dec, quote = FALSE, na = "", eol = "\n")
 
   ##### write SPSS syntax
   labels <- GADSdat$labels
   varInfo <- unique(labels[, c("varName", "varLabel", "format")])
-  valInfo <- unique(labels[!is.na(labels$value), c("varName", "value", "valLabel", "missings")])
+  valInfo <- unique(labels[!is.na(labels$value) & labels$missings == "valid", c("varName", "value", "valLabel", "missings")])
+  misInfo <- unique(labels[!is.na(labels$value) & labels$missings == "miss", c("varName", "value", "valLabel", "missings")])
 
-  #browser()
+  varnames <- gsub("[^[:alnum:]_\\$@#]", "\\.", colnames(GADSdat$dat))
+  dl.varnames <- varnames
+
+  chv <- sapply(GADSdat$dat, is.character)
+  lengths <- sapply(names(GADSdat$dat), function(ll) { if(is.numeric(GADSdat$dat[,ll]) & is.numeric(labels$value[labels$varName==ll])) {
+                            max(nchar(round(na.omit(abs(c(GADSdat$dat[,ll],labels$value[labels$varName==ll]))), digits=0)))
+                           } else {
+                            max(nchar(c(GADSdat$dat[,ll],labels$value[labels$varName==ll])))
+                            }
+                          })
+  decimals <- sapply(names(GADSdat$dat), function(ll) { if(is.numeric(GADSdat$dat[,ll]) & is.numeric(labels$value[labels$varName==ll])) {
+                            max(nchar(as.character(na.omit(abs(c(GADSdat$dat[,ll],labels$value[labels$varName==ll]))))))
+                          } else {
+                            max(nchar(c(GADSdat$dat[,ll],labels$value[labels$varName==ll])))
+                            }
+                          })
+
+  varsWithDecimals <-  names(which(lengths != decimals))
+
+  decimals2 <- sapply(varsWithDecimals, function(ll) {if(is.numeric(GADSdat$dat[,ll]) & is.numeric(labels$value[labels$varName==ll])) {
+max(nchar(na.omit(unlist(lapply(strsplit(as.character(na.omit(abs(c(GADSdat$dat[,ll],labels$value[labels$varName==ll])))),"\\."), function(b) b[2])))))
+                          } else {
+                            max(nchar(na.omit(unlist(lapply(strsplit(na.omit(c(GADSdat$dat[,ll],labels$value[labels$varName==ll])),"\\."), function(b) b[2])))))
+                            }
+                         })
+
+
+
+  if (any(chv)) {
+    lengths2 <- paste("(", ifelse(chv, "A", "F"), lengths, ")", sep = "")
+  }  else {
+    lengths2 <- paste ( "(F", lengths, ")", sep = "")
+  }
+
+  if(!is.null(varsWithDecimals)){
+    lengths2[which(dl.varnames %in% varsWithDecimals)] <-sapply(seq(along=lengths2[which(dl.varnames %in% varsWithDecimals)]), function(i) {
+    x <- paste0(decimals[varsWithDecimals][i], ".", decimals2[varsWithDecimals][i],")")
+    gsub("[0-9]+)$", x, lengths2[which(dl.varnames %in% varsWithDecimals)][i])
+    })
+  }
+  dl.varnames <- paste(dl.varnames, lengths2)
+
   # write header
   freefield <- " free (TAB)\n"
   cat("DATA LIST FILE=", autoQuote(filePath), freefield, file = syntaxPath)
-  cat(" /", varInfo$varName, ".\n\n", file = syntaxPath, append = TRUE,
+  cat(" /", dl.varnames, ".\n\n", file = syntaxPath, append = TRUE,
       fill = 60, labels = " ")
 
   # write variable labels
@@ -47,7 +89,7 @@ write_spss2.GADSdat <- function(GADSdat, filePath, syntaxPath) {
   if (nrow(valInfo) > 0) {
 
     cat("\nVALUE LABELS\n", file = syntaxPath, append = TRUE)
-
+# überlegen, ob für string variablen post hoc die labels$value auf character setzen
     for (v in unique(valInfo$varName)) {
       cat(" /", v, "\n", file = syntaxPath, append = TRUE)
       #if (any(nchar(valueLabels) > 120L)) {
@@ -61,7 +103,23 @@ write_spss2.GADSdat <- function(GADSdat, filePath, syntaxPath) {
   }
 
   # write missing codes
-  # tbd
+  if (nrow(misInfo) > 0) {
+
+    cat("\nMISSING VALUES\n", file = syntaxPath, append = TRUE)
+
+    for (v in unique(misInfo$varName)) {
+      if(isTRUE(chv[v])) {
+        cat(paste0(v, " ('",  paste(misInfo$value[misInfo$varName==v],collapse="','"), "')\n",  sep = " "),
+            file = syntaxPath, append = TRUE)
+
+      } else {
+      cat(paste0(v, " (",  paste(misInfo$value[misInfo$varName==v],collapse=","), ")\n",  sep = " "),
+          file = syntaxPath, append = TRUE)
+      }
+    }
+    cat(".\n", file = syntaxPath, append = TRUE)
+
+  }
 
   cat("\nEXECUTE.\n", file = syntaxPath, append = TRUE)
 
--- tests/testthat/test_writeSPSS2.R
+++ tests/testthat/test_writeSPSS2.R
@@ -1,16 +1,16 @@
 
 
 test_that("Write spss 2", {
-  g <- import_raw(df = data.frame(var1 = "abc", var2 = 1, stringsAsFactors = FALSE),
-                  varLabels = data.frame(varName = c("var1", "var2"), varLabel = c("a label", NA), stringsAsFactors = FALSE),
-                  valLabels = data.frame(varName = c("var1", "var1", "var2"), value = c(-96, -99, -99),
-                                         valLabel = c("miss1", "miss2", "miss1"),
-                                         missings = c("miss", "miss", "miss"), stringsAsFactors = FALSE))
+  g <- import_raw(df = data.frame(var1 = c("abc","bb"), var2 = c(1,0), var3 = c(1.01, 2.00), var4 = c(1.0001, 4.001),stringsAsFactors = FALSE),
+                  varLabels = data.frame(varName = c("var1", "var2", "var3", "var4"), varLabel = c("a label", NA, "another label", NA), stringsAsFactors = FALSE),
+                  valLabels = data.frame(varName = c("var1", "var1", "var2", "var2", "var2","var3", "var3"), value = c(-96, -99, -99, 0, 1,-96, -99),
+                                         valLabel = c("miss1", "miss2", "miss2","right","wrong","miss1", "miss2"),
+                                         missings = c("miss", "miss", "miss","valid","valid","miss", "miss"), stringsAsFactors = FALSE))
   g <- changeSPSSformat(g, varName = "var1", format = "A3")
   f_txt <- tempfile(fileext = ".txt")
   f_sps <- tempfile(fileext = ".sps")
-  write_spss2(g, filePath = f_txt, syntaxPath = f_sps)
+  write_spss2(g, filePath = f_txt, syntaxPath = f_sps,dec=",")
 
   out <- read.table(f_txt, stringsAsFactors = FALSE)
-  expect_equal(as.character(out), as.character(g$dat))
+  expect_equal(out, data.frame(V1 = c("abc","bb"), V2 = c(1,0), V3 = c("1,01", "2"), V4 = c("1,0001", "4,001"),stringsAsFactors = FALSE))
 })
