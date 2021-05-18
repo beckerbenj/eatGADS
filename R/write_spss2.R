db37c72718c67f8ebbef7e1a10fbd29b820157be
sachseka <sachseka@hu-berlin.de>
2021-05-18 15:56:26 +0200

    hth ubuntu

--- R/write_spss2.R
+++ R/write_spss2.R
@@ -196,7 +196,7 @@ autoQuote <- function (x){
 
 readMultisep <- function(file,sep) {
   lines <- readLines(file)
-  datf <- data.frame(do.call(rbind,strsplit(lines, sep, fixed = TRUE)))
-  datf <- data.frame(lapply(datf,utils::type.convert,as.is=TRUE))
+  datf <- data.frame(do.call(rbind,strsplit(lines, sep, fixed = TRUE)), stringsAsFactors=FALSE)
+  datf <- data.frame(lapply(datf,utils::type.convert,as.is=TRUE), stringsAsFactors=FALSE)
   return(datf)
 }
