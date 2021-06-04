a30195a406fec38330e540328d92f4b7789610e4
sachseka <sachseka@hu-berlin.de>
2021-06-04 15:32:33 +0200

    last test for writespss

--- tests/testthat/test_write_spss2.R
+++ tests/testthat/test_write_spss2.R
@@ -17,10 +17,6 @@ test_that("Write spss 2 overall", {
 
   syntax <- readChar(f_sps, file.info(f_sps)$size)
   expect_true(grepl("EXECUTE.", syntax))
-  expect_true(grepl("^DATA LIST FILE", syntax))
-  expect_true(grepl("VARIABLE LABELS", syntax))
-  expect_true(grepl("VALUE LABELS", syntax))
-  expect_true(grepl("MISSING VALUES", syntax))
 })
 
 
@@ -64,3 +60,46 @@ test_that("writeHeader", {
   expect_true(grepl("free", syntax))
   expect_true(grepl("var1 \\(A4\\) var2 \\(F2\\) var3 \\(F4.2\\) var4 \\(F6.4\\)", syntax))
 })
+
+
+
+test_that("writeVaLab", {
+  g <- import_raw(df = data.frame(var1 = c("ab c","bb"), var2 = c(1,0), var3 = c(1.01, 2.00), var4 = c(1.0001, 4.001),stringsAsFactors = FALSE),
+                  varLabels = data.frame(varName = c("var1", "var2", "var3", "var4"), varLabel = c("a label", NA, "another label", NA), stringsAsFactors = FALSE),
+                  valLabels = data.frame(varName = c("var1", "var1", "var2", "var2", "var2","var3", "var3"), value = c(-96, -99, -99, 0, 1,-96, -99),
+                                         valLabel = c("miss1", "miss2", "miss2","right","wrong","miss1", "miss2"),
+                                         missings = c("miss", "miss", "miss","valid","valid","miss", "miss"), stringsAsFactors = FALSE))
+  g <- changeSPSSformat(g, varName = "var1", format = "A3")
+  f_txt <- tempfile(fileext = ".txt")
+  f_sps <- tempfile(fileext = ".sps")
+  write_spss2(g, filePath = f_txt, syntaxPath = f_sps, dec=",")
+
+  syntax <- readChar(f_sps, file.info(f_sps)$size)
+
+  expect_true(grepl("VARIABLE LABELS", syntax))
+  expect_true(grepl("VALUE LABELS", syntax))
+  expect_true(grepl("var3 \"another label\"", syntax))
+  expect_true(grepl("var1 \r\n   -99 \"miss2\"", syntax))
+  expect_true(grepl("1 \"wrong\"", syntax))
+})
+
+
+test_that("writeMisCode", {
+  g <- import_raw(df = data.frame(var1 = c("ab c","bb"), var2 = c(1,0), var3 = c(1.01, 2.00), var4 = c(1.0001, 4.001),stringsAsFactors = FALSE),
+                  varLabels = data.frame(varName = c("var1", "var2", "var3", "var4"), varLabel = c("a label", NA, "another label", NA), stringsAsFactors = FALSE),
+                  valLabels = data.frame(varName = c("var1", "var1", "var2", "var2", "var2","var3", "var3"), value = c(-96, -99, -99, 0, 1,-96, -99),
+                                         valLabel = c("miss1", "miss2", "miss2","right","wrong","miss1", "miss2"),
+                                         missings = c("miss", "miss", "miss","valid","valid","miss", "miss"), stringsAsFactors = FALSE))
+  g <- changeSPSSformat(g, varName = "var1", format = "A3")
+  f_txt <- tempfile(fileext = ".txt")
+  f_sps <- tempfile(fileext = ".sps")
+  write_spss2(g, filePath = f_txt, syntaxPath = f_sps, dec=",")
+
+  syntax <- readChar(f_sps, file.info(f_sps)$size)
+
+  expect_true(grepl("MISSING VALUES", syntax))
+  expect_true(grepl("var1 \\('-99,-96'\\)", syntax))
+  expect_true(grepl("var2 \\(-99\\)", syntax))
+  expect_true(grepl("var3 \\(-99,-96\\)", syntax))
+})
+
