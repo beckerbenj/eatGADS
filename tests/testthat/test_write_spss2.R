66aaede50fa7f458ff209e8c7fdda7ce32e15811
sachseka <sachseka@hu-berlin.de>
2021-06-04 14:33:15 +0200

    write a test per day

--- tests/testthat/test_write_spss2.R
+++ tests/testthat/test_write_spss2.R
@@ -43,3 +43,24 @@ test_that("createInputWriteFunctions", {
   expect_true(identical(r1$dl.varnames, c("var1 (A4)", "var2 (F2)", "var3 (F4.2)", "var4 (F6.4)")))
   expect_true(identical(unname(r1$chv), c(TRUE, FALSE, FALSE, FALSE)))
 })
+
+
+
+
+test_that("writeHeader", {
+  g <- import_raw(df = data.frame(var1 = c("ab c","bb"), var2 = c(1,0), var3 = c(1.01, 2.00), var4 = c(1.0001, 4.001),stringsAsFactors = FALSE),
+                  varLabels = data.frame(varName = c("var1", "var2", "var3", "var4"), varLabel = c("a label", NA, "another label", NA), stringsAsFactors = FALSE),
+                  valLabels = data.frame(varName = c("var1", "var1", "var2", "var2", "var2","var3", "var3"), value = c(-96, -99, -99, 0, 1,-96, -99),
+                                         valLabel = c("miss1", "miss2", "miss2","right","wrong","miss1", "miss2"),
+                                         missings = c("miss", "miss", "miss","valid","valid","miss", "miss"), stringsAsFactors = FALSE))
+  g <- changeSPSSformat(g, varName = "var1", format = "A3")
+  f_txt <- tempfile(fileext = ".txt")
+  f_sps <- tempfile(fileext = ".sps")
+  write_spss2(g, filePath = f_txt, syntaxPath = f_sps, dec=",")
+
+  syntax <- readChar(f_sps, file.info(f_sps)$size)
+
+  expect_true(grepl("DATA LIST FILE=.", syntax))
+  expect_true(grepl("free", syntax))
+  expect_true(grepl("var1 \\(A4\\) var2 \\(F2\\) var3 \\(F4.2\\) var4 \\(F6.4\\)", syntax))
+})
