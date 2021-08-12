576f59237be38de1ed42eed95d28025ce4e85066
sachseka <sachseka@hu-berlin.de>
2021-08-12 17:45:31 +0200

    2nd change according t check4spss

--- R/write_spss2.R
+++ R/write_spss2.R
@@ -35,11 +35,11 @@ write_spss2.GADSdat <- function(GADSdat, filePath, syntaxPath, dec =".", fileEnc
   check_GADSdat_varLevel_meta(GADSdat)
   checkz <- check4SPSS(GADSdat)
 
-  if(length(checkz$varNames_special) > 0) stop(paste("Please remove special characters in variable names: ", checkz$varNames_special, collapse=" "))
-  if(length(checkz$varNames_length) > 0) stop(paste("Please shorten variable names to < 64 byte: ", checkz$varNames_length, collapse=" "))
-  if(length(checkz$varLabels) > 0) stop(paste("Please shorten variable labels to < 256 byte: ", checkz$varLabels, collapse=" "))
-  if(length(checkz$valLabels) > 0) stop(paste("Please shorten value labels to < 120 byte: ", checkz$valLabels, collapse=" "))
-  if(length(checkz$missings) > 0) message(paste("Too many missing values for character variables \'", checkz$missings,"\'. SPSS allows only three missing values for character variables. I will take the first 3.", collapse=" "))
+  if(length(checkz$varNames_special) > 0) stop("Please remove special characters in variable names: ", paste(checkz$varNames_special, collapse=" "))
+  if(length(checkz$varNames_length) > 0) stop("Please shorten variable names to < 64 byte: ", paste(checkz$varNames_length, collapse=" "))
+  if(length(checkz$varLabels) > 0) stop("Please shorten variable labels to < 256 byte: ", paste(checkz$varLabels, collapse=" "))
+  if(length(checkz$valLabels) > 0) stop("Please shorten value labels to < 120 byte: ", paste(names(checkz$valLabels), collapse=" "))
+  if(length(checkz$missings) > 0) message("Too many missing values for character variables: ", paste(checkz$missings, collapse= " "),". SPSS allows only three missing values for character variables. I will take the first 3.")
 
   GADSdat <- checkMissings(GADSdat)
   GADSdat <- checkFormat(GADSdat, ...)
