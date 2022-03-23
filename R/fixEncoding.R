a3dd0c5c70f18bc342fdb944d3c2a9e4369bc0ee
Benjamin Becker <benj.becker@web.de>
2022-03-23 17:12:13 +0100

    some work on fixEncoding

--- R/fixEncoding.R
+++ R/fixEncoding.R
@@ -2,9 +2,10 @@
 #############################################################################
 #' Fix encoding problems.
 #'
-#' Fix encoding problems of a \code{GADSdat} which was encoded presumably using \code{UTF-8} and imported using \code{ASCII} encoding.
+#' Fix some encoding problems of a character vector or a \code{GADSdat} object, which was encoded presumably using \code{UTF-8}
+#' and imported using \code{ASCII} encoding.
 #'
-#'@param x A \code{GADSdat} object.
+#'@param x A character vector or \code{GADSdat} object.
 #'
 #'@return The modified \code{GADSdat_imp}..
 #'
@@ -15,20 +16,38 @@
 fixEncoding <- function(x) {
   UseMethod("fixEncoding")
 }
-
+#'@export
 fixEncoding.GADSdat <- function(x) {
   check_GADSdat(x)
   GADSdat <- x
 
-  stop("Not implemented yet.")
+  # fix variable names
+  nam_lookup <- data.frame(oldNames = namesGADS(GADSdat), stringsAsFactors = FALSE)
+  nam_lookup[["newNames"]] <- fixEncoding(nam_lookup[["oldNames"]])
+  nam_lookup_diff <- nam_lookup[nam_lookup$oldNames != nam_lookup$newNames, ]
+  #browser()
+  for(oldNam in nam_lookup_diff$oldNames) {
+    newNam <- nam_lookup_diff[nam_lookup_diff$oldNames == oldNam, "newNames"]
+    GADSdat <- changeVarNames(GADSdat, oldNames = oldNam, newNames = newNam)
+  }
+
   # fix data
+  character_vars <- sapply(GADSdat$dat, is.character)
+  character_vars <- names(character_vars)[character_vars]
+  for(nam in character_vars) {
+    GADSdat$dat[[nam]] <- fixEncoding(GADSdat$dat[[nam]])
+  }
 
   # fix meta data
-  GADSdat_out
+  for(col_nam in c("varLabel", "valLabel")) {
+    GADSdat$labels[[col_nam]] <- fixEncoding(GADSdat$labels[[col_nam]])
+  }
+
+  GADSdat
 }
 
 
-
+#'@export
 fixEncoding.character <- function(x) {
   lookup <- data.frame(unicode = c("C\026", "C..\023", "C\034", "C..\\$", "C\\$", "C..6", "C6", "C..<", "C<", "C..8", "C\037", "\001", "\025"),
              substitute = c("Oe", "Ue", "Ue", "ae", "ae", "oe", "oe", "ue", "ue", "ss", "ss", "", ""),
--- tests/testthat/test_fixEncoding.R
+++ tests/testthat/test_fixEncoding.R
@@ -1,10 +1,24 @@
-# testM <- import_spss("tests/testthat/helper_spss_missings.sav")
-# testM <- import_spss("helper_spss_missings.sav")
-#
-# test_that("errors", {
-#   expect_error(calculateScale(testM, items = "VAR1", scale = "VARnew"),
-#                "'items' needs to be a character vector of at least length 2.")
-#   expect_error(calculateScale(testM, items = c("VAR1", "VAR2"), scale = c("VARnew", "VARnew2")),
-#                "'scale' needs to be a character vector of length 1.")
-# })
-#
+dat <- data.frame(v1 = c("kl", "mw", "hi"),
+                  v2 = 1:3,
+                  v3 = c("\001", "C\026", "C6"))
+g <- import_DF(dat)
+g <- changeVarLabels(g, varName = "v2", varLabel = c("This is a problem\001"))
+g <- changeValLabels(g, varName = "v2", value = 1, valLabel = c("This is a problemC...\023"))
+
+test_that("with character vectors", {
+  out <- fixEncoding(c("C..6", "ABC", "C\037"))
+  expect_equal(out, c("oe", "ABC", "ss"))
+})
+
+test_that("with GADS", {
+  out <- fixEncoding(g)
+  expect_equal(out$dat$v1, c("kl", "mw", "hi"))
+  expect_equal(out$dat$v2, 1:3)
+  expect_equal(out$dat$v3, c("", "Oe", "ss"))
+})
+
+test_that("with changes in namesGADS", {
+  g2 <- changeVarNames(g, oldNames = "v3", newNames = "vC\034")
+  out <- fixEncoding(g2)
+  expect_equal(namesGADS(out), c("v1", "v2", "vUe"))
+})
