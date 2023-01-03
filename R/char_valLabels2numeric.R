2816101faa5ae72e285270de3f8d0d6602d0cf1e
Benjamin Becker <benj.becker@web.de>
2023-01-03 15:34:03 +0100

    redo transform labeledStrings

--- R/char_valLabels2numeric.R
+++ R/char_valLabels2numeric.R
@@ -46,20 +46,33 @@ char_valLabels2numeric.savDat<- function(rawDat, labeledStrings) {
       }
     }
     if(identical(labeledStrings, "transform")) {
-      warning("Some values with value labels or missing tags of variable ", char_var,
-              " cannot be coerced to numeric. Therefore all underlying values are recoded to numeric.")
+      # create lookup table for all values
+      #if(char_var == "TR_KURS_DEU") browser()
+      all_oldValues <- unique(c(old_attributes$labels, old_attributes$na_values))
+      all_oldValues <- all_oldValues[all_oldValues != ""] ## hotfix to circumvent issues with missing tags for empty strings; in other circumstances these are silently dropped anyway
 
-      # create lookup table for alle values
-      oldValues <- unique(c(old_attributes$labels, old_attributes$na_values))
-      lookup <- data.frame(oldValues = oldValues,
-                           newValues = seq(length(oldValues)))
+      na_char_oldValues <- suppressWarnings(eatTools::asNumericIfPossible(x = all_oldValues, force.string = TRUE))
+      num_oldValues <- na_char_oldValues[!is.na(na_char_oldValues)]
+      char_oldValues <- all_oldValues[is.na(na_char_oldValues)]
+
+      # if no transformation is necessary move to next variable (e.g., only missing values are tagged & labelled)
+      if(length(char_oldValues) == 0) next
+
+      # assign former character values new numbers but skip already used numbers (prevent conflicts)
+      lookup <- data.frame(oldValues = char_oldValues,
+                           newValues = seq_but_skip(to = length(char_oldValues), skip = num_oldValues))
+
+      warning("Some values with value labels or missing tags of variable ", char_var,
+              " cannot be coerced to numeric. These string values are recoded to numeric.")
 
       if(!is.null(new_attributes$labels)) {
         new_attributes$labels <- eatTools::recodeLookup(new_attributes$labels, lookup)
+        new_attributes$labels <- as.numeric(new_attributes$labels)
         names(new_attributes$labels) <- names(old_attributes$labels)
       }
       if(!is.null(new_attributes$na_values)) {
         new_attributes$na_values <- eatTools::recodeLookup(new_attributes$na_values, lookup)
+        new_attributes$na_values <- as.numeric(new_attributes$na_values)
       }
 
       rawDat[[char_var]] <- eatTools::recodeLookup(rawDat[[char_var]], lookup)
@@ -89,4 +102,10 @@ can_be_numeric <- function(x) {
   numNAs <- sum(is.na(x))
   numNAs_new <- suppressWarnings(sum(is.na(as.numeric(x))))
   return(numNAs_new == numNAs)
+}
+
+seq_but_skip <- function(to, skip) {
+  x <- seq(from = 1, to = to + length(skip))
+  x2 <- setdiff(x, skip)
+  x2[seq(from = 1, to = to)]
 }
--- tests/testthat/test_char_valLabels2numeric.R
+++ tests/testthat/test_char_valLabels2numeric.R
@@ -11,6 +11,15 @@ string_miss_df <- tibble::tibble(v1 = 1:3, v2 = string_miss)
 string_combi <- haven::labelled_spss(c("a", "b", "c", 5), labels = c(alpha = "a", beta = "b", gamma = 5), na_values = c("c"))
 string_combi_df <- tibble::tibble(v1 = 1:4, v2 = string_combi)
 
+string_num_miss <- haven::labelled_spss(c("a", "b", "c", 5, -99), labels = c(alpha = "a", beta = "b", gamma = 5), na_values = c("c", -99))
+string_num_miss2 <- haven::labelled_spss(c("a", "b", "c", -98, -99), labels = c(omit = -98, nr = "-99"), na_values = c(-98, "-99"))
+string_num_miss_df <- tibble::tibble(v1 = 1:5, v2 = string_num_miss, v3 = string_num_miss2)
+
+test_that("Sequence but skip numbers", {
+  expect_equal(seq_but_skip(5, skip = c(2, 4)), c(1, 3, 5, 6, 7))
+  expect_equal(seq_but_skip(4, skip = 1:4), c(5:8))
+})
+
 test_that("No labeled character values", {
   expect_equal(char_valLabels2numeric.savDat(num_values_df, labeledStrings = "drop"), num_values_df)
 })
@@ -30,16 +39,20 @@ test_that("Labeled character values and drop value labels", {
 
 test_that("Labeled character values and transform value labels", {
   expect_warning(out1 <- char_valLabels2numeric.savDat(string_values_df, labeledStrings = "transform"),
-                 "Some values with value labels or missing tags of variable v2 cannot be coerced to numeric. Therefore all underlying values are recoded to numeric.")
+                 "Some values with value labels or missing tags of variable v2 cannot be coerced to numeric. These string values are recoded to numeric.")
   expect_equal(attributes(out1$v1), attributes(string_values_df$v1))
-  expect_equal(attributes(out1$v2)$labels, c(alpha = 1, beta = 2, gamma = 3))
-  expect_equal(as.character(out1[["v2"]]), c(1, 2, "c", 3))
+  expect_equal(attributes(out1$v2)$labels, c(alpha = 1, beta = 2, gamma = 5))
+  expect_equal(as.character(out1[["v2"]]), c(1, 2, "c", 5))
 })
 
 test_that("Missing tagged character values and keep value labels", {
   expect_warning(out1 <- char_valLabels2numeric.savDat(string_miss_df, labeledStrings = "keep"),
                  "Some values with value labels or missing tags of variable v2 cannot be coerced to numeric.")
   expect_equal(out1, string_miss_df)
+
+  expect_warning(out2 <- char_valLabels2numeric.savDat(string_num_miss_df, labeledStrings = "keep"),
+                 "Some values with value labels or missing tags of variable v2 cannot be coerced to numeric.")
+  expect_equal(out2, string_num_miss_df)
 })
 
 test_that("Missing tagged character values and drop value labels", {
@@ -50,22 +63,46 @@ test_that("Missing tagged character values and drop value labels", {
   expect_equal(attributes(out1$v2)$na_values, NA_real_)
 })
 
+test_that("Missing tagged and labeled character values and drop value labels", {
+  expect_warning(out2 <- char_valLabels2numeric.savDat(string_num_miss_df, labeledStrings = "drop"),
+                 "Some values with value labels or missing tags of variable v2 cannot be coerced to numeric and are therefore changed to NA.")
+  expect_equal(attributes(out2$v1), attributes(string_values_df$v1))
+  expect_equal(attributes(out2$v2)$labels, c(alpha = NA, beta = NA, gamma = 5))
+  expect_equal(attributes(out2$v2)$na_values, c(NA_real_, -99))
+})
+
 test_that("Missing tagged character values and transform value labels", {
   expect_warning(out1 <- char_valLabels2numeric.savDat(string_miss_df, labeledStrings = "transform"),
-                 "Some values with value labels or missing tags of variable v2 cannot be coerced to numeric. Therefore all underlying values are recoded to numeric.")
+                 "Some values with value labels or missing tags of variable v2 cannot be coerced to numeric. These string values are recoded to numeric.")
   expect_equal(attributes(out1$v1), attributes(string_miss_df$v1))
   expect_equal(attributes(out1$v2)$labels, c(alpha = 1, beta = 2))
   expect_equal(attributes(out1$v2)$na_values, 3)
-  expect_equal(as.numeric(out1[["v2"]]), c(1, 2, 3))
+
+  v2 <- out1[["v2"]]
+  attributes(v2) <- NULL
+  expect_equal(as.numeric(v2), c(1, 2, 3))
 })
 
 test_that("Missing tagged and labeled character values and transform value labels", {
   expect_warning(out1 <- char_valLabels2numeric.savDat(string_combi_df, labeledStrings = "transform"),
-                 "Some values with value labels or missing tags of variable v2 cannot be coerced to numeric. Therefore all underlying values are recoded to numeric.")
+                 "Some values with value labels or missing tags of variable v2 cannot be coerced to numeric. These string values are recoded to numeric.")
   expect_equal(attributes(out1$v1), attributes(string_combi_df$v1))
-  expect_equal(attributes(out1$v2)$labels, c(alpha = 1, beta = 2, gamma = 3))
-  expect_equal(attributes(out1$v2)$na_values, 4)
-  expect_equal(as.numeric(out1[["v2"]]), c(1, 2, 4, 3))
+  expect_equal(attributes(out1$v2)$labels, c(alpha = 1, beta = 2, gamma = 5))
+  expect_equal(attributes(out1$v2)$na_values, 3)
+  v2 <- out1[["v2"]]
+  attributes(v2) <- NULL
+  expect_equal(as.numeric(v2), c(1, 2, 3, 5))
+
+  expect_warning(out2 <- char_valLabels2numeric.savDat(string_num_miss_df, labeledStrings = "transform"),
+                 "Some values with value labels or missing tags of variable v2 cannot be coerced to numeric. These string values are recoded to numeric.")
+  expect_equal(attributes(out2$v1), attributes(string_num_miss_df$v1))
+  expect_equal(attributes(out2$v2)$labels, c(alpha = 1, beta = 2, gamma = 5))
+  expect_equal(attributes(out2$v2)$na_values, c(3, -99))
+  v2_2 <- out2[["v2"]]
+  attributes(v2_2) <- NULL
+  expect_equal(as.numeric(v2_2), c(1, 2, 3, 5, -99))
+  expect_equal(attributes(out2$v3)$labels, c(omit = -98, nr = "-99"))
+  expect_equal(attributes(out2$v3)$na_values, c(-98, "-99"))
 })
 
 #
