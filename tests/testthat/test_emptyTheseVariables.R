e0070a1b858ff955024b41f9cfa232a43a5743cb
Benjamin Becker <benj.becker@web.de>
2023-04-19 14:52:47 +0200

    provide emptyTheseVariables() for #19

---
+++ tests/testthat/test_emptyTheseVariables.R
@@ -0,0 +1,18 @@
+
+
+# dfSAV <- import_spss(file = "tests/testthat/helper_spss.sav")
+dfSAV <- import_spss(file = "helper_spss.sav")
+
+test_that("emptyTheseVariables for GADSdat", {
+  out1 <- emptyTheseVariables(dfSAV, vars = c("VAR1", "VAR2"))
+  expect_equal(out1$dat$VAR1, rep(NA_real_, 1))
+  expect_equal(out1$dat$VAR2, rep(NA_real_, 1))
+  expect_equal(out1$dat$VAR3, dfSAV$dat$VAR3)
+  out2 <- emptyTheseVariables(dfSAV, vars = "VAR3")
+  expect_equal(out2$dat$VAR3, rep(NA_character_, 1))
+
+  expect_silent(write_spss(out1, filePath = tempfile()))
+  expect_silent(write_spss(out2, filePath = tempfile()))
+})
+
+
