c2d334e112fd980584e455c6f36b10e5ffae5700
Nikolas Weigt <nikolas.daniel.weigt.1@iqb.hu-berlin.de>
2025-05-14 18:30:43 +0200

    add new check function for labeled integer values that are greater than .Machine$integer.max

---
+++ R/checkIntOverflow.R
@@ -0,0 +1,72 @@
+#' Check a \code{GADSdat} for large integers.
+#'
+#' Check a \code{GADSdat} object for any occurrences of integer values, esp.
+#' in the metadata, that are too large for R to handle.
+#' in its meta data which could cause problems esp. when exporting into \code{.dta} format.
+#'
+#' According to its documentation, R can only handle \code{\link[base:integer]{integers}}
+#' of up to \emph{roughly} \eqn{\pm 2 \times 10^9} (2,147,483,647 to be exact).
+#' Right now, this restriction appears to only be of interfering relevance when
+#' exporting a data set to \code{.dta} and only when the very large value is also
+#' labeled (or tagged as missing). This is due to Stata only accepting integers
+#' as being labeled - otherwise the value will stay a generic numeric.
+#'
+#'@param GADSdat A \code{GADSdat} object.
+#'
+#'@return Returns a \code{data.frame}, listing the affected \code{varName}s,
+#' the large \code{value}s, and their respective \code{missings} tag
+#' according to the metadata, as well as whether they actually occur in the data  (\code{empty}).
+#' The \code{rownum}s of the affected rows in \code{GADSdat$labels} are provided
+#' in a separate column as a fail safe, too.
+#'
+#'@examples
+#' # Introduce a fractional value into meta data
+#' pisa2 <- changeMissings(GADSdat = pisa,
+#'                         varName = "schtype",
+#'                         value = 9999999999,
+#'                         missings = "miss")
+#' checkIntOverflow(pisa2)
+#'
+#'@export
+checkIntOverflow <- function(GADSdat) {
+  check_GADSdat(GADSdat)
+  labels <- GADSdat$labels
+
+  out <- data.frame(varName = "<none found>",
+                    value = NA_real_,
+                    missings = NA_character_,
+                    empty = NA,
+                    rownum = NA_integer_)
+
+  # option 1
+  # huge_number_rows <- which((labels$labeled == "yes") &
+  #                            (labels$value > 2147483647 | labels$value < -2147483647))
+
+  # option 2
+  huge_number_rows <- suppressWarnings(which((labels$labeled == "yes") &
+                                               !is.na(as.numeric(labels$value)) &
+                                               is.na(as.integer(labels$value))))
+
+  if (length(huge_number_rows) == 0) {
+    return(out)
+  }
+
+  out[1:length(huge_number_rows), 1:3] <- labels[huge_number_rows, c("varName",
+                                                                     "value",
+                                                                     "missings")]
+  out$rownum <- huge_number_rows
+
+  varlist <- unique(out$varName)
+  empty_values <- checkEmptyValLabels(GADSdat = GADSdat,
+                                      vars = varlist)
+  out$empty <- unlist(lapply(varlist, function(varname) {
+    vallist <- out[out$varName == varname, "value"]
+    is_empty <- match(x = vallist,
+                      table = empty_values[[varname]]$value,
+                      nomatch = 0) > 0
+    return(is_empty)
+  }))
+
+  rownames(out) <- NULL
+  return(out)
+}
---
+++ tests/testthat/test_checkIntOverflow.R
@@ -0,0 +1,45 @@
+# load + generally prepare data
+dfSAV <- import_spss(file = test_path("helper_spss_missings.sav"))
+df1 <- changeValLabels(dfSAV, "VAR1", 2, "Apple")
+
+# general output structure to be changed for each test
+outlist <- data.frame(varName = "<none found>",
+                      value = NA_real_,
+                      missings = NA_character_,
+                      empty = NA,
+                      rownum = NA_integer_)
+
+test_that("Correctly identify metadata without very large labeled values", {
+  check_result <- checkIntOverflow(df1)
+  expect_equal(check_result, outlist)
+})
+
+test_that("Two existing labeled value exactly 1 above/below the limit in one variable", {
+  df2 <- recodeGADS(df1, "VAR1", c(1,-99), c(2147483648, -2147483648), "ignore")
+  check_result <- checkIntOverflow(df2)
+  outlist2 <- outlist
+  outlist2[1,] <- list("VAR1", -2147483648, "miss", FALSE, 1)
+  outlist2[2,] <- list("VAR1", 2147483648, "valid", FALSE, 4)
+  expect_equal(check_result, outlist2)
+})
+
+test_that("Only empty very large labeled values", {
+  df2 <- changeValLabels(df1, "VAR1", c(-9999999999, 9999999999), c("mbddd", "other"))
+  df2 <- changeMissings(df2, "VAR1", c(-9999999999, 9999999999), c("miss", "valid"))
+  check_result <- checkIntOverflow(df2)
+  outlist2 <- outlist
+  outlist2[1,] <- list("VAR1", -9999999999, "miss", TRUE, 1)
+  outlist2[2,] <- list("VAR1", 9999999999, "valid", TRUE, 6)
+  expect_equal(check_result, outlist2)
+})
+
+# remove this last one?
+test_that("Two very large labeled values in the same variable where one value is empty", {
+  df2 <- recodeGADS(df1, "VAR1", 2, 9999999999, "ignore")
+  df2 <- changeValLabels(df2, "VAR1", 20000000000, "nothing")
+  check_result <- checkIntOverflow(df2)
+  outlist2 <- outlist
+  outlist2[1,] <- list("VAR1", 9999999999, "valid", FALSE, 4)
+  outlist2[2,] <- list("VAR1", 20000000000, "valid", TRUE, 5)
+  expect_equal(check_result, outlist2)
+})
