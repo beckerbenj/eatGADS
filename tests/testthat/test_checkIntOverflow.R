756f4a6d8b131b886167ca23a7b4ad493f7f985b
Nikolas Weigt <nikolas.daniel.weigt.1@iqb.hu-berlin.de>
2025-05-19 18:18:36 +0200

    add argument names to all functions used in tests (maybe more than necessary, but it won't hurt)

--- tests/testthat/test_checkIntOverflow.R
+++ tests/testthat/test_checkIntOverflow.R
@@ -1,6 +1,9 @@
 # load + generally prepare data
 dfSAV <- import_spss(file = test_path("helper_spss_missings.sav"))
-df1 <- changeValLabels(dfSAV, "VAR1", 2, "Apple")
+df1 <- changeValLabels(GADSdat = dfSAV,
+                       varName = "VAR1",
+                       value = 2,
+                       valLabel = "Apple")
 
 # general output structure to be changed for each test
 outlist <- data.frame(varName = "<none found>",
@@ -10,13 +13,17 @@ outlist <- data.frame(varName = "<none found>",
                       rownum = NA_integer_)
 
 test_that("Correctly identify metadata without very large labeled values", {
-  check_result <- checkIntOverflow(df1)
+  check_result <- checkIntOverflow(GADSdat = df1)
   expect_equal(check_result, outlist)
 })
 
 test_that("Two existing labeled value exactly 1 above/below the limit in one variable", {
-  df2 <- recodeGADS(df1, "VAR1", c(1,-99), c(2147483648, -2147483648), "ignore")
-  check_result <- checkIntOverflow(df2)
+  df2 <- recodeGADS(GADSdat = df1,
+                    varName = "VAR1",
+                    oldValues = c(1,-99),
+                    newValues = c(2147483648, -2147483648),
+                    existingMeta = "ignore")
+  check_result <- checkIntOverflow(GADSdat = df2)
   outlist2 <- outlist
   outlist2[1,] <- list("VAR1", -2147483648, "miss", FALSE, 1)
   outlist2[2,] <- list("VAR1", 2147483648, "valid", FALSE, 4)
@@ -24,9 +31,15 @@ test_that("Two existing labeled value exactly 1 above/below the limit in one var
 })
 
 test_that("Only empty very large labeled values", {
-  df2 <- changeValLabels(df1, "VAR1", c(-9999999999, 9999999999), c("mbddd", "other"))
-  df2 <- changeMissings(df2, "VAR1", c(-9999999999, 9999999999), c("miss", "valid"))
-  check_result <- checkIntOverflow(df2)
+  df2 <- changeValLabels(GADSdat = df1,
+                         varName = "VAR1",
+                         value = c(-9999999999, 9999999999),
+                         valLabel = c("mbddd", "other"))
+  df2 <- changeMissings(GADSdat = df2,
+                        varName = "VAR1",
+                        value = c(-9999999999, 9999999999),
+                        missings = c("miss", "valid"))
+  check_result <- checkIntOverflow(GADSdat = df2)
   outlist2 <- outlist
   outlist2[1,] <- list("VAR1", -9999999999, "miss", TRUE, 1)
   outlist2[2,] <- list("VAR1", 9999999999, "valid", TRUE, 6)
@@ -35,9 +48,16 @@ test_that("Only empty very large labeled values", {
 
 # remove this last one?
 test_that("Two very large labeled values in the same variable where one value is empty", {
-  df2 <- recodeGADS(df1, "VAR1", 2, 9999999999, "ignore")
-  df2 <- changeValLabels(df2, "VAR1", 20000000000, "nothing")
-  check_result <- checkIntOverflow(df2)
+  df2 <- recodeGADS(GADSdat = df1,
+                    varName = "VAR1",
+                    oldValues = 2,
+                    newValues = 9999999999,
+                    existingMeta = "ignore")
+  df2 <- changeValLabels(GADSdat = df2,
+                         varName = "VAR1",
+                         value = 20000000000,
+                         valLabel = "nothing")
+  check_result <- checkIntOverflow(GADSdat = df2)
   outlist2 <- outlist
   outlist2[1,] <- list("VAR1", 9999999999, "valid", FALSE, 4)
   outlist2[2,] <- list("VAR1", 20000000000, "valid", TRUE, 5)
--- tests/testthat/test_checkLabeledFractionals.R
+++ tests/testthat/test_checkLabeledFractionals.R
@@ -1,6 +1,9 @@
 # load + generally prepare data
 dfSAV <- import_spss(file = test_path("helper_spss_missings.sav"))
-df1 <- changeValLabels(dfSAV, "VAR1", 2, "Apple")
+df1 <- changeValLabels(GADSdat = dfSAV,
+                       varName = "VAR1",
+                       value = 2,
+                       valLabel = "Apple")
 
 # general output structure to be changed for each test
 outlist <- data.frame(varName = "<none found>",
@@ -9,22 +12,32 @@ outlist <- data.frame(varName = "<none found>",
                       empty = NA)
 
 test_that("Correctly identify metadata without labeled fractional values", {
-  check_result <- checkLabeledFractionals(df1)
+  check_result <- checkLabeledFractionals(GADSdat = df1)
   expect_equal(check_result, outlist)
 })
 
 test_that("One existing labeled fractional in one variable", {
-  df2 <- recodeGADS(df1, "VAR1", 1, .5, "ignore")
-  check_result <- checkLabeledFractionals(df2)
+  df2 <- recodeGADS(GADSdat = df1,
+                    varName = "VAR1",
+                    oldValues = 1,
+                    newValues = .5,
+                    existingMeta = "ignore")
+  check_result <- checkLabeledFractionals(GADSdat = df2)
   outlist2 <- outlist
   outlist2[1,] <- list("VAR1", .5, "valid", FALSE)
   expect_equal(check_result, outlist2)
 })
 
 test_that("Only empty labeled fractionals", {
-  df2 <- changeValLabels(df1, "VAR1", c(.2, -94.94), c("point two", "mbd"))
-  df2 <- changeMissings(df2, "VAR1", c(.2, -94.94), c("valid", "miss"))
-  check_result <- checkLabeledFractionals(df2)
+  df2 <- changeValLabels(GADSdat = df1,
+                         varName = "VAR1",
+                         value = c(.2, -94.94),
+                         valLabel = c("point two", "mbd"))
+  df2 <- changeMissings(GADSdat = df2,
+                        varName = "VAR1",
+                        value = c(.2, -94.94),
+                        missings = c("valid", "miss"))
+  check_result <- checkLabeledFractionals(GADSdat = df2)
   outlist2 <- outlist
   outlist2[1,] <- list("VAR1", -94.94, "miss", TRUE)
   outlist2[2,] <- list("VAR1", .2, "valid", TRUE)
@@ -33,9 +46,16 @@ test_that("Only empty labeled fractionals", {
 
 # remove this last one?
 test_that("Two labeled fractionals in the same variable where one value is empty", {
-  df2 <- recodeGADS(df1, "VAR1", 2, .5555555, "ignore")
-  df2 <- changeValLabels(df2, "VAR1", .000001, "nothing")
-  check_result <- checkLabeledFractionals(df2)
+  df2 <- recodeGADS(GADSdat = df1,
+                    varName = "VAR1",
+                    oldValues = 2,
+                    newValues = .5555555,
+                    existingMeta = "ignore")
+  df2 <- changeValLabels(GADSdat = df2,
+                         varName = "VAR1",
+                         value = .000001,
+                         valLabel = "nothing")
+  check_result <- checkLabeledFractionals(GADSdat = df2)
   outlist2 <- outlist
   outlist2[1,] <- list("VAR1", .000001, "valid", TRUE)
   outlist2[2,] <- list("VAR1", .5555555, "valid", FALSE)
