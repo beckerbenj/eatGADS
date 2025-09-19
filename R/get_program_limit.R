d35749bf888841ef0aacd1de5be15bbd0fc54cc8
Nikolas Weigt <nikolas.daniel.weigt.1@iqb.hu-berlin.de>
2025-09-19 22:36:02 +0200

    get_program_limits: add option to return all limits

--- R/get_program_limit.R
+++ R/get_program_limit.R
@@ -15,41 +15,59 @@
 #'  \item \code{nrows}: number of observations
 #'  \item \code{ncols}: number of variables
 #' }
+#' Run \code{get_program_limit()} to see all limits that are collected in this function.
 #'
 #' @references \href{https://www.stata.com/products/comparison-of-limits/}{Stata: Comparison of limits}
 #'
-#' @param component Single string. Which limits should be returned?
 #' @param version Optional. Overwrites \code{program} with a specific version of a program (see details).
 #' @param program Character vector. Name(s) of the program(s) whose limits should be returned.
+#' @param component Optional single string. Which limits should be returned? If this is "all" or
+#'  not specified, all limits for the chosen \code{program}(s) are returned.
 #'
 #' @returns A list of two elements: \code{x} (numeric size of the limit) and
-#'  \code{unit} ("char", "byte", or "generic"). If more than one \code{program} is specified,
-#'  the most restrictive limit will be returned.
+#'  \code{unit} ("char", "byte", or "generic"). If more than one \code{program} is specified and
+#'  \code{component} is not "all", the most restrictive limit will be returned.
 #'
 #' @family dataset compliance checks
 #'
 #' @examples
-#' get_program_limit("SPSS", "varname")
+#' # Get all limits that Stata imposes on its datasets
+#' eatGADS::get_program_limit("Stata", "all")
+#'
+#' # Get the specific limit on variable name lengths under SPSS
+#' eatGADS::get_program_limit("SPSS", "varNames")
 get_program_limit <- function(program = c("SPSS", "Stata"),
-                              component = c("varname", "varlabel", "vallabel", "stringvar",
-                                            "rows", "cols"),
+                              component = c("all",
+                                            "varNames", "varLabels", "valLabels", "stringvars",
+                                            "nrows", "ncols"),
                               version = NULL) {
   program <- match.arg(program, several.ok = TRUE)
-  check_characterArgument(component)
+  if (component[[1]] == "all") {
+    component <- "all"
+  } else {
+    component <- match.arg(component, several.ok = FALSE)
+  }
   if (!is.null(version)) check_characterArgument(version)
 
   program_limits <- list(x = matrix(c(64, 256, 120, 32767, 2^31-1, 2^31-1,
                                       32, 80, 32, 2*10^6, 2^31-29, 32767),
                                     ncol = 2, nrow = 6,
-                                    dimnames = list(c("varname", "varlabel", "vallabel", "stringvar",
-                                                      "rows", "cols"),
                                                     c("SPSS", "Stata"))),
+                                    dimnames = list(c("varNames", "varLabels", "valLabels", "stringvars",
+                                                      "nrows", "ncols"),
                          unit = matrix(c("byte", "char", "char", "byte", "generic", "generic",
                                          "char", "char", "char", "byte", "generic", "generic"),
                                        ncol = 2, nrow = 6,
-                                       dimnames = list(c("varname", "varlabel", "vallabel", "stringvar",
-                                                         "rows", "cols"),
                                                        c("SPSS", "Stata"))))
+                                       dimnames = list(c("varNames", "varLabels", "valLabels", "stringvars",
+                                                         "nrows", "ncols"),
+  if (component == "all") {
+    filtered_x <- program_limits$x[, program]
+    filtered_unit <- program_limits$unit[, program]
+    return(list(x = filtered_x,
+                unit = filtered_unit))
+  }
+
   x <- unit <- NULL
   for (i in seq_along(program)) {
     newx <- program_limits$x[component, program[[i]]]
--- tests/testthat/test_get_program_limit.R
+++ tests/testthat/test_get_program_limit.R
@@ -1,5 +1,5 @@
 test_that("Returns a list of 1 numeric and 1 character", {
-  output <- get_program_limit("Stata", "varname")
+  output <- get_program_limit("Stata", "varNames")
   # list level
   expect_true(is.list(output))
   expect_equal(length(output), 2)
@@ -14,11 +14,17 @@ test_that("Return most restrictive limit", {
   expect_spss <- list(x = 64, unit = "byte")
   expect_stata <- list(x = 32, unit = "char")
 
-  result_spss <- get_program_limit("SPSS", "varname")
-  result_stata <- get_program_limit("Stata", "varname")
-  result_joint <- get_program_limit(c("SPSS", "Stata"), "varname")
+  result_spss <- get_program_limit("SPSS", "varNames")
+  result_stata <- get_program_limit("Stata", "varNames")
+  result_joint <- get_program_limit(c("SPSS", "Stata"), "varNames")
 
   expect_equal(result_spss, expect_spss)
   expect_equal(result_stata, expect_stata)
   expect_equal(result_joint, expect_stata)
+})
+
+test_that("Complete output of all limits", {
+  output <- get_program_limit()
+  expect_equal(dim(output$x), c(6, 2))
+  expect_equal(dim(output$x), dim(output$unit))
 })
