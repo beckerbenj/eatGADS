b921c548017860055f3a1fd00ecbc0b88ae49e3a
Nikolas Weigt <nikolas.daniel.weigt.1@iqb.hu-berlin.de>
2025-11-24 20:22:40 +0100

    checkMissingValLabels: separate test
    Move tests for checkMissingValLabels to a separate file in preparation for the test for checkValLabels
    
    #110

---
+++ tests/testthat/test_checkMissingValLabels.R
@@ -0,0 +1,122 @@
+
+dfSAV <- import_spss(file = test_path("helper_spss_missings.sav"))
+load(file = test_path("helper_data.rda"))
+
+df3 <- df2
+df3$dat[1, 1:2] <- 8
+
+iris2 <- iris
+iris2[, "charVar"] <- "test"
+suppressMessages(iris_g <- import_DF(iris2))
+
+test_that("Input validation", {
+  expect_error(checkMissingValLabels(df1, vars = 3:4),
+               "The following 'vars' are not variables in the GADSdat: 3, 4")
+  expect_error(checkMissingValLabels(df1, valueRange = 3:5),
+               "'valueRange' needs to be a numeric vector of length 2.")
+  expect_error(checkMissingValLabels(df1, valueRange = letters[3:4]),
+               "'valueRange' needs to be a numeric vector of length 2.")
+})
+
+test_that("give_GADSdat_classes", {
+  out <- give_GADSdat_classes(dfSAV)
+  expect_equal(out, c(VAR1 = "integer", VAR2 = "integer", VAR3 = "integer"))
+
+  suppressMessages(dfSAV_mini <- extractVars(dfSAV, vars = "VAR1"))
+  out2 <- give_GADSdat_classes(dfSAV_mini)
+  expect_equal(out2, c(VAR1 = "integer"))
+
+  out3 <- give_GADSdat_classes(iris_g)
+  expect_equal(out3, c(Sepal_Length = "double", Sepal_Width = "double",
+                       Petal_Length = "double", Petal_Width = "double",
+                       Species = "integer", charVar = "character"))
+
+  out4 <- give_GADSdat_classes(pisa, vars = c("g8g9", "age"))
+  expect_equal(out4, c(g8g9 = "integer", age = "double"))
+})
+
+test_that("give_GADSdat_classes and long integers", {
+  long_int <- import_DF(data.frame(ID = c(5401239857, 5401239858, 5401239850)))
+  long_int2 <- import_DF(data.frame(ID = c(NA, 5401239858, 5401239850)))
+  long_int3 <- import_DF(data.frame(ID = c(1, 5401239858, 5401239850)))
+  long_int4 <- import_DF(data.frame(ID = c(5401239858, 5401239850, 5401239850.4)))
+
+  out <- give_GADSdat_classes(long_int)
+  expect_equal(out, c(ID = "integer"))
+
+  out2 <- give_GADSdat_classes(long_int2)
+  expect_equal(out2, c(ID = "integer"))
+
+  out3 <- give_GADSdat_classes(long_int3)
+  expect_equal(out3, c(ID = "integer"))
+
+  out4 <- give_GADSdat_classes(long_int4)
+  expect_equal(out4, c(ID = "double"))
+})
+
+test_that("checkMissingValLabels", {
+  out <- checkMissingValLabels(dfSAV)
+  expect_equal(names(out), paste0("VAR", 1:3))
+  expect_equal(names(out[[1]]), c("varLabel", "missing_labels"))
+  expect_equal(names(out[[2]]), c("varLabel", "missing_labels"))
+  expect_equal(out[[1]]$varLabel, "Variable 1")
+  expect_equal(out[[3]]$varLabel, "Variable 3")
+  expect_equal(out[[1]]$missing_labels, 2)
+  expect_equal(out[[2]]$missing_labels, 1)
+
+  dfSAV2 <- removeValLabels(dfSAV, "VAR1", value = c(-99, -96, 1))
+  out2 <- checkMissingValLabels(dfSAV2)
+  expect_equal(out2[[1]]$missing_labels, c(-99, -96, 1, 2))
+
+  dfSAV3 <- changeValLabels(dfSAV, "VAR1", value = c(2), valLabel = "test")
+  out3 <- checkMissingValLabels(dfSAV3)
+  expect_equal(out3[[1]], NULL)
+
+  out4 <- checkMissingValLabels(iris_g)
+  expect_equal(out4, list(Species = NULL))
+
+  out5 <- checkMissingValLabels(pisa, vars = c("g8g9", "age"))
+  expect_equal(out5, list(g8g9 = NULL))
+})
+
+test_that("checkMissingValLabels data.frame", {
+  out <- checkMissingValLabels(dfSAV, output = "data.frame")
+  expect_equal(names(out), c("variable", "varLabel", "number_of_missing_labels", "values_with_missing_labels"))
+  expect_equal(out$variable, paste0("VAR", 1:3))
+  expect_equal(out$number_of_missing_labels, rep(1, 3))
+  expect_equal(out$values_with_missing_labels, c("2", "1", "1"))
+
+  dfSAV2 <- removeValLabels(dfSAV, "VAR1", value = c(-99, -96, 1))
+  out2 <- checkMissingValLabels(dfSAV2, output = "data.frame")
+  expect_equal(out2$number_of_missing_labels, c(4, 1, 1))
+  expect_equal(out2$values_with_missing_labels, c("-99, -96, 1, 2", "1", "1"))
+
+  dfSAV3 <- changeValLabels(dfSAV, "VAR1", value = c(2), valLabel = "test")
+  out3 <- checkMissingValLabels(dfSAV3, output = "data.frame")
+  expect_equal(out3$variable, c("VAR2", "VAR3"))
+
+  dfLong <- data.frame(v1 = factor(letters[1:12]))
+  gLong <- import_DF(dfLong)
+  gLong <- removeValLabels(gLong, "v1", value = 1:12)
+  out4 <- checkMissingValLabels(gLong, output = "data.frame")
+  expect_equal(out4$values_with_missing_labels, paste0(paste(1:10, collapse = ", "), ", ..."))
+})
+
+
+test_that("With NAs", {
+  suppressMessages(suppressWarnings(dfSAV2 <- recode2NA(dfSAV, value = 1)))
+  out <- checkMissingValLabels(dfSAV2)
+  expect_equal(out[[1]], checkMissingValLabels(dfSAV)[[1]])
+  expect_equal(out[[2]], NULL)
+  expect_equal(out[[3]], NULL)
+})
+
+
+test_that("with specific value range", {
+  out <- checkMissingValLabels(dfSAV, valueRange = c(3, -100))
+  expect_equal(out, checkMissingValLabels(dfSAV))
+
+  out2 <- checkMissingValLabels(dfSAV, valueRange = c(1, -100))
+  expect_equal(out2[[2]], out2[[2]])
+  expect_equal(out2[[1]], NULL)
+})
