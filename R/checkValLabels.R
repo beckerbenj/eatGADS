73325cd27e06bad0d1fc37ec1707b528119bcfee
Nikolas Weigt <nikolas.daniel.weigt.1@iqb.hu-berlin.de>
2025-11-24 21:30:21 +0100

    checkValLabels: first stable version
    Add the core functionalities of the new checkValLabels function. Tests will have to be added, and examples are missing yet.
    
    #110

--- R/checkValLabels.R
+++ R/checkValLabels.R
@@ -9,28 +9,86 @@
 #' If more than one program name is given in \code{charLimits}, the most restrictive limit will be
 #'  applied. For details about program specific limits, see \link{program_limits}.
 #'
+#' Please note that setting \code{printLength} to \code{NULL} (and thereby deactivating label
+#'  truncation) might not actually result in the printing of the full length of the exceedingly
+#'  long labels if you are using RStudio. The program's own limits to how many characters are
+#'  printed to the console may still apply
+#'  (see \href{https://stackoverflow.com/questions/36800475/avoid-string-printed-to-console-getting-truncated-in-rstudio}{Stack Overflow}).
+#'
 #'@param GADSdat A \code{GADSdat} object.
 #'@param charLimits Character vector of the program(s) against whose limit(s) the labels should
 #' be checked.
 #'@param vars Optional character vector of the variables whose value labels should be checked.
 #' By default, all value labels will be checked.
+#'@param printLength Single numeric value. The first n = \code{printLength} characters will be
+#' reported. Alternatively, specify \code{NULL} to deactivate the truncation and have the function
+#' report the full labels (not recommended; see details).
 #'
-#'@return Returns a \code{data.frame}, listing every \code{valLabel} that exceeds the limit and its
-#' character length, along with the \code{varName}, \code{value}, and whether the value the label
-#' is attached to actually occurs in the data (\code{empty}).
+#'@return Returns a \code{data.frame} with every (truncated) long \code{valLabel} along with the
+#' \code{varName} and \code{value}, with which it occurs, its character length (\code{charLength}),
+#' and whether the value the label is attached to actually occurs in the data (\code{empty}).
 #'
 #'@examples
 #' to be added
 #'
 #'@export
-checkValLabels <- function(GADSdat, charLimits = c("SPSS", "Stata"), vars = namesGADS(GADSdat)) {
+checkValLabels <- function(GADSdat, charLimits = c("SPSS", "Stata"),
+                           vars = namesGADS(GADSdat), printLength = 40) {
   check_GADSdat(GADSdat)
   program <- match.arg(charLimits, several.ok = TRUE)
   check_vars_in_GADSdat(GADSdat, vars = vars)
+  if (!is.null(printLength)) {
+    check_numericArgument(printLength)
+  }
 
   out <- data.frame(varName = character(),
                     value = numeric(),
                     valLabel = character(),
                     charLength = numeric(),
                     empty = logical())
+  limit_list <- getProgramLimit(program = program,
+                                component = "valLabels")
+  all_meta <- extractMeta(GADSdat, vars = vars)
+  vars_without_valLabels <- NULL
+
+  for (single_var in vars) {
+    single_meta <- all_meta[all_meta$varName == single_var,]
+    # any valLabels to check?
+    if (all(is.na(single_meta$valLabel))) {
+      vars_without_valLabels <- c(vars_without_valLabels, single_var)
+      next
+    }
+
+    # any long labels to report?
+    single_var_valLabels <- single_meta$valLabel
+    label_lengths <- nchar(single_var_valLabels, type = limit_list$unit)
+    long_label_pointer <- which(label_lengths > limit_list$value)
+    if (identical(length(long_label_pointer), 0L)) {
+      next
+    }
+
+    if (!is.null(printLength)) {
+      long_labels <- unlist(lapply(single_var_valLabels[long_label_pointer],
+                                   truncate_string,
+                                   n = printLength, unit = "char", suffix = ""))
+    }
+
+    add_out <- single_meta[long_label_pointer, c("varName", "value")]
+    add_out$valLabel <- long_labels
+    add_out$charLength <- label_lengths[long_label_pointer]
+    add_out$empty <- FALSE
+
+    # any empty long labels?
+    empty_valLabels <- checkEmptyValLabels(GADSdat, vars = single_var)[[single_var]]$value
+    if (!is.null(empty_valLabels) && any(empty_valLabels %in% add_out$value)) {
+      add_out[add_out$value %in% empty_valLabels] <- TRUE
+    }
+    out <- rbind(out, add_out)
+  }
+
+  if (!is.null(vars_without_valLabels) && !identical(length(vars), ncol(GADSdat$dat))) {
+    message("The following variables do not have any value labels to check: ",
+            vars_without_valLabels)
+  }
+  return(out)
 }
--- tests/testthat/test_checkValLabels.R
+++ tests/testthat/test_checkValLabels.R
@@ -1,11 +1,19 @@
-
 dfSAV <- import_spss(file = test_path("helper_spss_missings.sav"))
 load(file = test_path("helper_data.rda"))
 
-df3 <- df2
-df3$dat[1, 1:2] <- 8
-
-iris2 <- iris
-iris2[, "charVar"] <- "test"
-suppressMessages(iris_g <- import_DF(iris2))
-
+test_that("Correctly identify long value labels", {
+  stata_limit <- getProgramLimit("Stata", "valLabels")$value
+  gads_meta <- dfSAV$labels
+  gads_meta$valLabel[1] <- paste0(rep("a", stata_limit + 1),
+                                  collapse = "")
+  gads <- dfSAV
+  gads$labels <- gads_meta
+  out <- checkValLabels(GADSdat = gads, charLimits = "Stata", printLength = 40)
+  expected_out <- data.frame(varName = "VAR1",
+                             value = -99,
+                             valLabel = paste0(rep("a", 40),
+                                               collapse = ""),
+                             charLength = stata_limit + 1,
+                             empty = FALSE)
+  expect_equal(out, expected_out)
+})
