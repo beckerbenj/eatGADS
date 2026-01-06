ad117dffd5ce30c80b5600a83b2c376c29e577f4
Nikolas Weigt <nikolas.daniel.weigt.1@iqb.hu-berlin.de>
2026-01-06 13:38:39 +0100

    check_functions: omit function call from errors
    Set call. in stop to FALSE to omit the function calls from the error messages.

--- R/check_functions.R
+++ R/check_functions.R
@@ -1,24 +1,28 @@
 check_vars_in_GADSdat <- function(GADSdat, vars, argName = "vars", GADSdatName = "GADSdat") {
   dup_vars <- vars[duplicated(vars)]
   if(length(dup_vars) > 0) stop("There are duplicates in '", argName,"': ",
-                                paste(dup_vars, collapse = ", "))
+                                paste(dup_vars, collapse = ", "),
+                                call. = FALSE)
 
   nams <- namesGADS(GADSdat)
   if(is.list(nams)) nams <- unlist(nams)
   other_vars <- vars[!vars %in% nams]
   if(length(other_vars) > 0) stop("The following '", argName,"' are not variables in the ", GADSdatName, ": ",
-                                  paste(other_vars, collapse = ", "))
+                                  paste(other_vars, collapse = ", "),
+                                  call. = FALSE)
   return()
 }
 
 check_vars_in_vector <- function(vec, vars, vec_nam) {
   dup_vars <- vars[duplicated(vars)]
   if(length(dup_vars) > 0) stop("There are duplicates in 'vars': ",
-                                paste(dup_vars, collapse = ", "))
+                                paste(dup_vars, collapse = ", "),
+                                call. = FALSE)
 
   other_vars <- vars[!vars %in% vec]
   if(length(other_vars) > 0) stop("The following 'vars' are not variables in the ", vec_nam, ": ",
-                                  paste(other_vars, collapse = ", "))
+                                  paste(other_vars, collapse = ", "),
+                                  call. = FALSE)
   return()
 }
 
@@ -27,7 +31,8 @@ check_logicalArgument <- function(arg, argName) {
     argName <- deparse(substitute(arg))
   }
   if(!is.logical(arg) || length(arg) != 1) {
-    stop("'", argName, "' needs to be a logical vector of length 1.")
+    stop("'", argName, "' needs to be a logical vector of length 1.",
+         call. = FALSE)
   }
   return()
 }
@@ -37,7 +42,8 @@ check_characterArgument <- function(arg, argName) {
     argName <- deparse(substitute(arg))
   }
   if(!is.character(arg) || length(arg) != 1) {
-    stop("'", argName, "' needs to be a character vector of length 1.")
+    stop("'", argName, "' needs to be a character vector of length 1.",
+         call. = FALSE)
   }
   return()
 }
@@ -47,7 +53,8 @@ check_numericArgument <- function(arg, argName) {
     argName <- deparse(substitute(arg))
   }
   if(!is.numeric(arg) || length(arg) != 1) {
-    stop("'", argName, "' needs to be a numeric vector of length 1.")
+    stop("'", argName, "' needs to be a numeric vector of length 1.",
+         call. = FALSE)
   }
   return()
 }
